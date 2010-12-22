8ff77971445fb2e9aa857d40f882f95884dfdb5c
Sven Fuchs <svenfuchs@artweb-design.de>
2010-12-22 03:47:27 +0100

    use dynamic dates

--- config/application.rb
+++ config/application.rb
@@ -14,8 +14,8 @@ module TravisRails
       Dir[root.join(dir.to_s).join('**/*.js')].map { |file| file.sub("#{root.to_s}/", '') }
     end
 
-    vendor  = %w(jquery-1.4.4.min.js jquery-ui-1.8.6.min.js underscore.js handlebars.js backbone.js socky.js)
-    jasmine = %w(jasmine.js jasmine-html.js)
+    vendor  = %w(jquery-1.4.4.min.js jquery-ui-1.8.6.min.js jquery.timeago.js underscore handlebars backbone socky)
+    jasmine = %w(jasmine jasmine-html)
     expansions = javascript_expansions(:lib, :app, :tests)
     expansions.merge!(:vendor => vendor.map { |name| "vendor/#{name}" })
     expansions.merge!(:jasmine => jasmine.map { |name| "vendor/#{name}" })
