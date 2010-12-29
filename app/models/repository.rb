8d0184297989a5e217c041ed49715c714b5e8689
Sven Fuchs <svenfuchs@artweb-design.de>
2010-12-29 17:15:41 +0100

    don't include builds into the timeline that haven't been started yet

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -9,7 +9,7 @@ class Repository < ActiveRecord::Base
   class << self
     def timeline
       # should probably denormalize the last build attributes which are relevant to the timeline at some point
-      includes(:last_build).order(Build.arel_table[:started_at].desc)
+      includes(:last_build).where(Build.arel_table[:started_at].not_eq(nil)).order(Build.arel_table[:started_at].desc)
     end
   end
 
