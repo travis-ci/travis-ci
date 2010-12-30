0be5402dfc27be951c2b7139dd13869a6fdc9481
Sven Fuchs <svenfuchs@artweb-design.de>
2010-12-30 16:25:17 +0100

    allow per-environment travis config files (not sure i like this)

--- lib/travis.rb
+++ lib/travis.rb
@@ -4,7 +4,12 @@ module Travis
 
   class << self
     def config
-      @config ||= YAML.load_file(File.expand_path('../../config/travis.yml', __FILE__))
+      @config ||= YAML.load_file(config_file)
+    end
+
+    def config_file
+      files = [Rails.env, nil].map { |env| File.expand_path(['../../config/travis', env, 'yml'].compact.join('.'), __FILE__) }
+      files.detect { |file| File.exist?(file) }
     end
   end
 end
