c8213a6353cfd610e8825992034ad97aa3ee8f50
Sven Fuchs <svenfuchs@artweb-design.de>
2011-01-10 18:17:57 +0100

    fix build history tests

--- public/javascripts/tests/views/build_history_test.js
+++ public/javascripts/tests/views/build_history_test.js
@@ -1,31 +1,27 @@
 describe('The build history tab', function() {
   beforeEach(function() {
-    go_to('#!/repositories/' +  + INIT_DATA.repositories[1].id + '/builds/' + INIT_DATA.repositories[1].last_build.id + '/history');
+    go_to('#!/repositories/' +  + INIT_DATA.repositories[1].id + '/history');
   });
 
   it('shows build details', function() {
-    runs_after(0, function() {
-      expect_texts('#right .build', {
+    runs_after(200, function() {
+      expect_texts('#right .repository', {
         'h3': 'josevalim/enginex',
-        'h4': 'Build #1',
-        '.commit-hash': '565294c',
-        '.commit-message': 'Update Capybara',
-        '.duration': '20 seconds',
+        '.summary .number': '1',
+        '.summary .commit-hash': '565294c',
+        '.summary .commit-message': 'Update Capybara',
+        '.summary .duration': '20 seconds',
         '.log': 'enginex build 1 log ...'
       });
     });
   });
 
   it('shows a builds list', function() {
-    runs_after(0, function() {
-      expect_texts('#right .build', {
-        'h3': 'josevalim/enginex',
-        'h4': 'Build #1',
-        '.commit-hash': '565294c',
-        '.commit-message': 'Update Capybara',
-        '.duration': '20 seconds',
-        '.log': 'enginex build 1 log ...'
-      });
+    runs_after(200, function() {
+      expect_table('#right #builds', [
+        ['Build', 'Duration',   'Finished'],
+        ['#1',    '20 seconds', '2010-11-11T12:00:20Z' ]
+      ]);
     });
   });
 });
