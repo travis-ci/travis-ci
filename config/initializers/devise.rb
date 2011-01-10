dbee03076d9b4b60f3cddfc6877a5497abe1b85b
Sven Fuchs <svenfuchs@artweb-design.de>
2011-01-10 18:17:21 +0100

    recover from undefined travis oauth2 config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,5 +2,5 @@ require 'travis'
 require 'devise_oauth2_authenticatable'
 require 'devise/orm/active_record'
 
-Devise::OAUTH2_CONFIG = Travis.config['oauth2']
+Devise::OAUTH2_CONFIG = Travis.config['oauth2'] || {}
 
