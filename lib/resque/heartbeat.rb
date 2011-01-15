ca34fe8c80c2be27b976408dadc753ea641b6283
Sven Fuchs <svenfuchs@artweb-design.de>
2011-01-15 16:36:21 +0100

    add a heartbeat to resque workers

---
+++ lib/resque/heartbeat.rb
@@ -0,0 +1,36 @@
+require 'resque'
+
+module Resque
+  def self.prune_dead_workers
+    Worker.all.each do |worker|
+      worker.unregister_worker if worker.last_heartbeat_before?(5)
+    end
+  end
+
+  class Worker
+    def startup_with_heartbeat
+      startup_without_heartbeat
+      Thread.new do
+        loop do
+          sleep(2)
+          heartbeat!
+        end
+      end
+    end
+    alias startup_without_heartbeat startup
+    alias startup startup_with_heartbeat
+
+    def heartbeat!
+      redis.sadd(:workers, self)
+      redis.set("worker:#{self}:heartbeat", Time.now.to_s)
+    end
+
+    def last_heartbeat_before?(seconds)
+      Time.parse(last_heartbeat).utc < (Time.now.utc - seconds)
+    end
+
+    def last_heartbeat
+      redis.get("worker:#{self}:heartbeat") || started
+    end
+  end
+end
