248d8964f53cdaedfe1ac4976f1aacd026ee57eb
Sven Fuchs <svenfuchs@artweb-design.de>
2011-02-02 15:47:09 +0100

    use :login as oauth2_uid field

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,6 +3,7 @@ require 'devise_oauth2_authenticatable'
 require 'devise/orm/active_record'
 
 Devise::OAUTH2_CONFIG = Travis.config['oauth2'] || {}
+Devise.oauth2_uid_field = 'login'
 
 Devise.setup do |config|
   config.http_authenticatable = true
