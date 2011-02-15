2b0f6fdee77c80291f1110c05c99049fdce15f42
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-02-15 14:32:42 +0100

    tweaking of the refraction rules

--- config/initializers/refraction_rules.rb
+++ config/initializers/refraction_rules.rb
@@ -3,7 +3,11 @@ Refraction.configure do |req|
   case req.host
   when /travis\.heroku\.com\/builds/
     # passthrough with no change (old github webhook)
+  when /travis\.heroku\.com/
+    # old heroku address
+    req.permanent! :host => "travis-ci.org"
   when /([-\w]+\.)+travis-ci\.org/
+    # we don't want to use www for now
     req.permanent! :host => "travis-ci.org"
   else
     # passthrough with no change
