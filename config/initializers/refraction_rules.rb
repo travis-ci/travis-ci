3a1611507d63c83a64dc897dadd06a50c2e51216
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-02-15 14:56:37 +0100

    and some corrections again to the rewrite rules for webhooks

--- config/initializers/refraction_rules.rb
+++ config/initializers/refraction_rules.rb
@@ -1,16 +1,16 @@
 Refraction.configure do |req|
 
-  case req.host
-  when /travis\.heroku\.com\/builds/
-    # passthrough with no change (old github webhook)
-    Rails.logger.add(1, "\n\ngithub webhook passthrough on old heroku address\n\n")
+  if req.host =~ /travis\.heroku\.com/
+    if req.path =~ /^\/builds/
+      # passthrough with no change (old github webhook)
+      Rails.logger.add(1, "\n\nPASSTHROUGH : github webhook passthrough on old heroku address\n\n")
+    else
+      # old heroku address
+      Rails.logger.add(1, "\n\nREDIRECT : redirect issued for old heroku address\n\n")
+      req.permanent! :host => "travis-ci.org"
+    end
     Rails.logger.flush
-  when /travis\.heroku\.com/
-    # old heroku address
-    Rails.logger.add(1, "\n\nredirect issued for old heroku address\n\n")
-    Rails.logger.flush
-    req.permanent! :host => "travis-ci.org"
-  when /([-\w]+\.)+travis-ci\.org/
+  elsif req.host =~ /([-\w]+\.)+travis-ci\.org/
     # we don't want to use www for now
     req.permanent! :host => "travis-ci.org"
   else
