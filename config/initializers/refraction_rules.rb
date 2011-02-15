29921f8746e5518f36c5ad230e8579f1fbeb756a
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-02-15 14:44:37 +0100

    a little more debugging

--- config/initializers/refraction_rules.rb
+++ config/initializers/refraction_rules.rb
@@ -3,10 +3,10 @@ Refraction.configure do |req|
   case req.host
   when /travis\.heroku\.com\/builds/
     # passthrough with no change (old github webhook)
-    Rails.logger.log "github webhook passthrough on old heroku address"
+    Rails.logger.add(:info, "github webhook passthrough on old heroku address")
   when /travis\.heroku\.com/
     # old heroku address
-    Rails.logger.log "redirect issued for old heroku address"
+    Rails.logger.add(:info, "redirect issued for old heroku address")
     req.permanent! :host => "travis-ci.org"
   when /([-\w]+\.)+travis-ci\.org/
     # we don't want to use www for now
