68d76891437d85021835673b273865e1c1a833ef
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-02-15 14:48:47 +0100

    and remember to flush the logger

--- config/initializers/refraction_rules.rb
+++ config/initializers/refraction_rules.rb
@@ -3,10 +3,12 @@ Refraction.configure do |req|
   case req.host
   when /travis\.heroku\.com\/builds/
     # passthrough with no change (old github webhook)
-    Rails.logger.add(1, "github webhook passthrough on old heroku address")
+    Rails.logger.add(1, "\n\ngithub webhook passthrough on old heroku address\n\n")
+    Rails.logger.flush
   when /travis\.heroku\.com/
     # old heroku address
-    Rails.logger.add(1, "redirect issued for old heroku address")
+    Rails.logger.add(1, "\n\nredirect issued for old heroku address\n\n")
+    Rails.logger.flush
     req.permanent! :host => "travis-ci.org"
   when /([-\w]+\.)+travis-ci\.org/
     # we don't want to use www for now
