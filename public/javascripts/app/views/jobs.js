c462fce2a4fbc558e1e4ea8d9435d8001ab0e3c8
Sven Fuchs <svenfuchs@artweb-design.de>
2011-02-17 18:01:04 +0100

    Revert "Revert ""thank you sponsors" box""
    This reverts commit 5287cc5d3acec579cbc5f8d1a645b9f9743eb40c.

--- public/javascripts/app/views/jobs.js
+++ public/javascripts/app/views/jobs.js
@@ -1,13 +1,15 @@
 Travis.Views.Jobs = Backbone.View.extend({
   initialize: function(args) {
-    _.bindAll(this, 'bind', 'unbind', 'render', 'renderItems', 'renderItem', 'jobAdded', 'jobRemoved', 'updateEmpty');
+    _.bindAll(this, 'element', 'bind', 'unbind', 'render', 'renderItems', 'renderItem', 'jobAdded', 'jobRemoved', 'updateEmpty');
 
     this.jobs = args.app.jobs;
     this.templates = {
       list: args.app.templates['jobs/list'],
       item: args.app.templates['jobs/_item']
     }
-    // this.element = $('#right');
+  },
+  element: function() {
+    return $('#jobs');
   },
   bind: function() {
     Backbone.Events.bind.apply(this, arguments);
@@ -23,10 +25,9 @@ Travis.Views.Jobs = Backbone.View.extend({
   },
   render: function() {
     this.bind();
-    $('#right #jobs').remove();
-    $('#right').append($(this.templates.list({})));
-    this.element = $('#right #jobs');
-    $('.loading', this.element).show();
+    this.element().empty();
+    this.element().append($(this.templates.list({ workers: [] })));
+    $('.loading', this.element()).show();
     this.renderItems(this.jobs);
   },
   renderItems: function(jobs) {
@@ -36,7 +37,7 @@ Travis.Views.Jobs = Backbone.View.extend({
     // Travis.Helpers.Util.updateTimes(this.element);
   },
   renderItem: function(job) {
-    $('ul', this.element).prepend($(this.templates.item(job.toJSON())));
+    $('ul', this.element()).prepend($(this.templates.item(job.toJSON())));
   },
   jobAdded: function(job) {
     this.updateEmpty();
@@ -44,10 +45,10 @@ Travis.Views.Jobs = Backbone.View.extend({
   },
   jobRemoved: function(job) {
     this.updateEmpty();
-    $('#job_' + job.get('meta_id'), this.element).remove();
+    $('#job_' + job.get('meta_id'), this.element()).remove();
   },
   updateEmpty: function() {
-    var element = $('.empty', this.element);
+    var element = $('.empty', this.element());
     this.jobs.length == 0 ? element.show() : element.hide();
   }
 });
--- public/javascripts/app/views/workers.js
+++ public/javascripts/app/views/workers.js
@@ -1,10 +1,15 @@
 Travis.Views.Workers = Backbone.View.extend({
   initialize: function(args) {
-    _.bindAll(this, 'bind', 'unbind', 'render', 'renderItems', 'renderItem', 'workerAdded', 'workerRemoved', 'updateEmpty');
+    _.bindAll(this, 'element', 'bind', 'unbind', 'render', 'renderItems', 'renderItem', 'workerAdded', 'workerRemoved', 'updateEmpty');
 
     this.workers = args.app.workers;
-    this.listTemplate = args.app.templates['workers/list'];
-    this.itemTemplate = args.app.templates['workers/_item'];
+    this.templates = {
+      list: args.app.templates['workers/list'],
+      item: args.app.templates['workers/_item']
+    }
+  },
+  element: function() {
+    return $('#workers');
   },
   bind: function() {
     Backbone.Events.bind.apply(this, arguments);
@@ -18,28 +23,27 @@ Travis.Views.Workers = Backbone.View.extend({
   },
   render: function() {
     this.bind();
-    $('#right #workers').remove();
-    $('#right').append($(this.listTemplate({ workers: [] })));
-    this.element = $('#right #workers');
-    $('.loading', this.element).show();
+    this.element().empty();
+    this.element().append($(this.templates.list({ workers: [] })));
+    $('.loading', this.element()).show();
     this.workers.load(this.renderItems);
   },
   renderItems: function(workers) {
-    $('.loading', this.element).hide();
+    $('.loading', this.element()).hide();
     this.updateEmpty();
     _.each(workers.models, function(worker) { this.renderItem(worker) }.bind(this));
   },
   renderItem: function(worker) {
-    $('ul', this.element).prepend(this.itemTemplate(worker.toJSON()));
+    $('ul', this.element()).prepend(this.templates.item(worker.toJSON()));
   },
   workerAdded: function(worker) {
     this.renderItem(worker);
   },
   workerRemoved: function(worker) {
-    $('#worker_' + worker.get('meta_id'), this.element).remove();
+    $('#worker_' + worker.get('meta_id'), this.element()).remove();
   },
   updateEmpty: function() {
-    var element = $('.empty', this.element);
+    var element = $('.empty', this.element());
     this.workers.length == 0 ? element.show() : element.hide();
   }
 });
