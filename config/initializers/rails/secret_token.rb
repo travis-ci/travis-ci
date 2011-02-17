e4400e559aba990bfc19407214eb30eb5cef1bff
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-02-17 23:55:35 +0100

    change the secret as devise sessions have changed (this resets the sessions and cookies)

--- config/initializers/rails/secret_token.rb
+++ config/initializers/rails/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TravisRails::Application.config.secret_token = '0be1bc014443b7d0ec43d08363ea201fb775eb34cd8e736f92a94fdd31db53441b3a6cf2a1e2aef9518f2a5384641b02da4128dc1d79319b49cbb94ac6bdcc4b'
+TravisRails::Application.config.secret_token = '0be1bc014443b7d0ec43d18363ea201fb875eb34cd8e736f92a94fdd31db53441b3a6cf2a1e2aef9518f2a5384641b02da4128dc1d79319b49cbb94ac6bdcc4b'
