48fe66e7a3d549290d6869d426ee4da85d4bbf3f
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-02-17 19:32:55 +0100

    some minor cleanups to the user model relating to the omniauth change

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,20 +5,29 @@ class User < ActiveRecord::Base
 
   has_many :tokens
 
-  def self.find_for_github_oauth(access_token)
-    data = access_token['extra']['user_hash']
+  before_save :create_a_token
+
+
+  def self.find_for_github_oauth(user_hash)
+    data = user_hash['extra']['user_hash']
     if user = User.find_by_login(data["login"])
       user
     else
-      user = create!(data.slice(*%w(name login email)))
-      user.tokens.create!
+      create!(data.slice(*%w(name login email)))
     end
-    user
   end
 
   def profile_image_hash
     self.email? ? Digest::MD5.hexdigest(self.email) : '00000000000000000000000000000000'
   end
+
+
+  private
+
+    def create_a_token
+      self.tokens.create! if self.tokens.empty?
+    end
+
 end
 
 
