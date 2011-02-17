98a704cdaca5d1492290672c4300c93e31514ef6
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-02-17 23:55:52 +0100

    remove the user scope, the less we ask for the better (for now)

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,5 +6,6 @@ Devise.setup do |config|
   require 'devise/orm/active_record'
 
   config.http_authenticatable = true
-  config.omniauth :github, OAUTH2_CONFIG['client_id'], OAUTH2_CONFIG['client_secret']
+
+  config.omniauth :github, OAUTH2_CONFIG['client_id'], OAUTH2_CONFIG['client_secret'], :scope => ''
 end
