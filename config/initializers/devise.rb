f96e7a36521b9a85fd604ad141618f88cfb8e640
beawesomeinstead <beawesomeinstead@yahoo.com>
2011-02-17 18:54:37 +0200

    Don't keep OAUTH2_CONFIG in Devise module.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,10 +1,9 @@
 require 'travis'
 require 'devise/orm/active_record'
 
-Devise::OAUTH2_CONFIG = Travis.config['oauth2'] || {}
-#Devise.oauth2_uid_field = 'login'
+OAUTH2_CONFIG = Travis.config['oauth2'] || {}
 
 Devise.setup do |config|
   config.http_authenticatable = true
-  config.omniauth :github, Devise::OAUTH2_CONFIG['client_id'], Devise::OAUTH2_CONFIG['client_secret']
+  config.omniauth :github, OAUTH2_CONFIG['client_id'], OAUTH2_CONFIG['client_secret']
 end
