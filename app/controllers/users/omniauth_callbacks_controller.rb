8abd432ca0c8ec9d861ce2d9299fa579d3c23ba1
beawesomeinstead <beawesomeinstead@yahoo.com>
2011-02-17 18:48:50 +0200

    Do not store authentication data in session.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,13 +1,12 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
-    @user = User.find_for_github_oauth(env["omniauth.auth"], current_user)
+    @user = User.find_for_github_oauth(env["omniauth.auth"])
 
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "GitHub"
       sign_in @user, :event => :authentication
       redirect_to root_url
     else
-      session["devise.github_data"] = env["omniauth.auth"]
       redirect_to root_url
     end
   end
