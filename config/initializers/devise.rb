06549dc7f4da3f5ac28aebdae395dd9fb312692f
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-02-18 01:43:35 +0100

    oops, removed a required require

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,6 +4,9 @@ require 'devise/api_token_authenticatable'
 OAUTH2_CONFIG = Travis.config['oauth2'] || {}
 
 Devise.setup do |config|
+  require 'devise/orm/active_record'
+
   config.http_authenticatable = true
+
   config.omniauth :github, OAUTH2_CONFIG['client_id'], OAUTH2_CONFIG['client_secret'], :scope => ''
 end
