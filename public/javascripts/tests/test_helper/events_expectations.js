3d3c8ac93f19f08307b21e910639df37123f43bd
Sven Fuchs <svenfuchs@artweb-design.de>
2011-02-18 08:13:32 +0100

    clean up js/test event expectations

--- public/javascripts/tests/test_helper/events_expectations.js
+++ public/javascripts/tests/test_helper/events_expectations.js
@@ -6,7 +6,7 @@ var itAddsTheRepositoryToTheRepositoriesCollection = function() {
   });
 };
 
-var itMovesTheRepositoryToTheTopOfTheRepositoriesList = function(delay) {
+var itMovesTheRepositoryToTheTopOfTheRepositoriesList = function() {
   it('prepends the repository to the repositories list', function() {
     runsWhen(repositoriesListPopulated, function() {
       expectText('#repositories .repository:nth-of-type(1) a:nth-of-type(1)', this.data.repository.name)
@@ -14,15 +14,15 @@ var itMovesTheRepositoryToTheTopOfTheRepositoriesList = function(delay) {
   });
 };
 
-var itUpdatesTheRepositoryListItemsBuildInformation = function(delay) {
+var itUpdatesTheRepositoryListItemsBuildInformation = function() {
   it('updates the build number of the repository list item', function() {
-    runsWhen(function() { return true; }, function() {
+    runs(function() {
       expectText('#repositories #repository_' + this.data.repository.id + ' .build', '#' + this.data.repository.last_build.number);
     });
   });
 }
 
-var itAddsTheBuildToTheRepositorysBuildsCollection = function(delay) {
+var itAddsTheBuildToTheRepositorysBuildsCollection = function() {
   it("it adds the build to the repository's builds collection", function() {
     runs(function() {
       expect(Travis.app.repositories.last().builds.models.length).toEqual(2);
@@ -30,97 +30,99 @@ var itAddsTheBuildToTheRepositorysBuildsCollection = function(delay) {
   });
 };
 
-var itSetsTheRepositoryListItemsBuildStatusColor = function(delay) {
+var itSetsTheRepositoryListItemsBuildStatusColor = function() {
   it('updates the build status color of the repository list item', function() {
-    runsAfter(delay, function() {
+    runs(function() {
       var selector = '#repositories #repository_' + this.data.repository.id + '.';
       expectElement(selector + 'red,' + selector + 'green');
     });
   });
 };
 
-var itResetsTheRepositoryListItemsBuildStatusColor = function(delay) {
+var itResetsTheRepositoryListItemsBuildStatusColor = function() {
   it('updates the build status color of the repository list item', function() {
-    runsAfter(delay, function() {
+    runs(function() {
       var selector = '#repositories #repository_' + this.data.repository.id + '.';
       expectNoElement(selector + 'red,' + selector + 'green');
     });
   });
 };
 
-var itIndicatesTheRepositoryIsBeingBuilt = function(delay) {
+var itIndicatesTheRepositoryIsBeingBuilt = function() {
   it('it indicates the repository is being built', function() {
-    runsAfter(delay, function() {
+    runs(function() {
       expectElement('#repositories #repository_' + this.data.repository.id + '.active');
     });
   });
 };
 
-var itDoesNotIndicateTheRepositoryIsBeingBuilt = function(delay) {
+var itDoesNotIndicateTheRepositoryIsBeingBuilt = function() {
   it('it does not indicate the repository is being built', function() {
-    runsAfter(delay, function() {
+    runs(function() {
       expectNoElement('#repositories #repository_' + this.data.repository.id + '.active');
     });
   });
 };
 
-var itUpdatesTheBuildSummary = function(delay) {
+var itUpdatesTheBuildSummary = function() {
   it('updates the build number of the repository build summary', function() {
-    runsAfter(delay, function() {
+    runs(function() {
       expectText('#main .summary .number', this.data.repository.last_build.number + '');
     });
   });
 };
 
-var itDoesNotUpdateTheBuildSummary = function(delay) {
+var itDoesNotUpdateTheBuildSummary = function() {
   it('does not update the build number of the repository build summary', function() {
-    runsAfter(delay, function() {
+    runs(function() {
       expectText('#main .summary .number', INIT_DATA.repositories[1].last_build.number + '');
     });
   });
 };
 
-var itAppendsToTheBuildLog = function(delay) {
+var itAppendsToTheBuildLog = function() {
   it('appends to the build log', function() {
-    runsAfter(delay, function() {
+    runs(function() {
       expectText('#main .log', this.repository.last_build.log + this.data.append_log)
     });
   });
 };
 
-var itDoesNotAppendToTheBuildLog = function(delay) {
+var itDoesNotAppendToTheBuildLog = function() {
   it('does not append to the build log', function() {
-    runsAfter(delay, function() {
+    runs(function() {
       expectText('#main .log', INIT_DATA.repositories[1].last_build.log)
     });
   });
 };
 
-var itPrependsTheBuildToTheBuildsHistoryTable = function(delay) {
+var itPrependsTheBuildToTheBuildsHistoryTable = function() {
   it('prepends the build to the builds history table', function() {
-    expectText('#main #builds tr:nth-child(2) td.number', '#' + this.data.number)
+    runs(function() {
+      expectText('#main #builds tr:nth-child(2) td.number', '#' + this.data.number)
+    });
   });
 };
 
-var itDoesNotPrependTheBuildToTheBuildsHistoryTable = function(delay) {
+var itDoesNotPrependTheBuildToTheBuildsHistoryTable = function() {
   it('does not prepend the build to the builds history table', function() {
-    runsAfter(delay, function() {
+    runs(function() {
       expectText('#main #builds tr:nth-child(2) td.number', '#' + this.repository.last_build.number)
     });
   });
 }
 
-var itUpdatesTheBuildsHistoryTableRow = function(delay) {
+var itUpdatesTheBuildsHistoryTableRow = function() {
   it('updates the builds history table row', function() {
-    runsAfter(400, function() {
+    runs(function() {
       expectAttributeValue('#main #builds tr:nth-child(2) td.finishedAt', 'title', this.data.finishedAt)
     });
   });
 };
 
-var itDoesNotUpdateTheBuildsHistoryTableRow = function(delay) {
+var itDoesNotUpdateTheBuildsHistoryTableRow = function() {
   it('does not update the builds history table row', function() {
-    runsAfter(400, function() {
+    runs(function() {
       expectAttributeValue('#main #builds tr:nth-child(2) td.finishedAt', 'title', this.repository.last_build.finishedAt)
     });
   });
