254a13c43569a16d1ff5b459960144752c73a7f6
beawesomeinstead <graf.otodrakula@gmail.com>
2011-02-19 18:56:46 +0200

    Remove that WebSocketServer autoload since we don't use it anymore.

--- lib/travis.rb
+++ lib/travis.rb
@@ -2,7 +2,6 @@ module Travis
   autoload :Buildable, 'travis/buildable'
   autoload :Builder,   'travis/builder'
   autoload :Config,    'travis/config'
-  autoload :WebSocketServer, 'travis/web_socket_server'
 
   class << self
     def config
