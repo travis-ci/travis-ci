435091e32066fa638e427fc7315cba303f294f65
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-02-20 18:54:48 +0100

    moved the json options for repository to a constant

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -6,6 +6,9 @@ class Repository < ActiveRecord::Base
   has_one :last_success, :class_name => 'Build', :order => 'started_at DESC', :conditions => { :status => 0 }
   has_one :last_failure, :class_name => 'Build', :order => 'started_at DESC', :conditions => { :status => 1 }
 
+  REPOSITORY_ATTRS = [:id, :name, :url, :last_duration]
+  LAST_BUILD_ATTRS = [:id, :number, :commit, :message, :status, :log, :started_at, :finished_at, :author_name, :author_email, :committer_name, :committer_email]
+
   class << self
     def timeline
       # should probably denormalize the last build attributes which are relevant to the timeline at some point
@@ -20,14 +23,18 @@ class Repository < ActiveRecord::Base
   before_create :init_names
 
   def as_json(options = {})
-    repository_keys = [:id, :name, :url, :last_duration]
-    last_build_options = { :only => [:id, :number, :commit, :message, :status, :log, :started_at, :finished_at, :author_name, :author_email, :committer_name, :committer_email] }
-    super(:only => repository_keys, :include => { :last_build => last_build_options })
+    super({
+      :only => REPOSITORY_ATTRS,
+      :include => { :last_build => { :only => LAST_BUILD_ATTRS } }
+    })
   end
 
+
   protected
+
     def init_names
       self.name ||= URI.parse(url).path.split('/')[-2, 2].join('/')
       self.username = name.split('/').first
     end
+
 end
