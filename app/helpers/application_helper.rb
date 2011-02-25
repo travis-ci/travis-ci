094a6cfb7ba4cc4ae540c2a701734fe903771423
beawesomeinstead <graf.otodrakula@gmail.com>
2011-02-25 06:17:18 +0200

    Simplify #active_page? helper by using routes-like API.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
-  def active_page?(pages = {})
-    is_active = pages.include?(params[:controller]) || pages.include?(params[:controller].split('/')[0])
-    is_active = pages[params[:controller]].include?(params[:action]) if is_active && pages[params[:controller]] && !pages[params[:controller]].empty?
-    is_active
+  def active_page?(page)
+    controller, action = page.split('#')
+    params[:controller] == controller && params[:action] == action
   end
 end
