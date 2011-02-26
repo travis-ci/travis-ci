ef6f7450f584cea6323a9774c9e6c8918fcf3e02
beawesomeinstead <graf.otodrakula@gmail.com>
2011-02-26 14:13:23 +0200

    Don't escape finished email body. Closes #30.

---
+++ test/unit/build_mailer_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class BuildMailerTest < ActionMailer::TestCase
+  def test_finished_email
+    build = Factory(:build, :committer_email => "chunkybacon@example.com", :log => "From git://github.com/bai/travis\n  f4822cb..8947caa  master     -> origin/master")
+    email = BuildMailer.finished_email(build).deliver
+    assert !ActionMailer::Base.deliveries.empty?
+
+    assert_equal [ build.committer_email ], email.to
+    assert_equal "svenfuchs/minimal# (62aae5f): the build has failed", email.subject
+    assert_match /master     -> origin\/master/, email.encoded
+  end
+end
