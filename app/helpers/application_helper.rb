09ab0494b8c288bcced34419eb850b9d1f1f5bde
rubiii <me@rubiii.com>
2011-02-28 23:13:29 +0100

    refined the use of gravatars
    * added rounded-borders to gravatar images
    * added a large gravatar to the profile page
    * changed the LI on the profile page to a DL

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
     controller, action = page.split('#')
     params[:controller] == controller && params[:action] == action
   end
+
+  def gravatar(user, options = {})
+    settings = { :size => 48 }.merge(options)
+    image_tag("http://www.gravatar.com/avatar/#{user.profile_image_hash}?s=#{settings[:size]}&d=mm", :alt => user.name, :class => "profile-avatar")
+  end
 end
