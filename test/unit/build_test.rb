88779b750206c8ec31a9c70d17350eec9fc6ad68
Sven Fuchs <svenfuchs@artweb-design.de>
2011-03-22 18:33:15 +0100

    refactor github api support so we can update repository owner email adresses more easily

--- test/unit/build_test.rb
+++ test/unit/build_test.rb
@@ -1,11 +1,14 @@
 require 'test_helper_rails'
 
 class BuildTest < ActiveSupport::TestCase
+  include GithubApiTestHelper
+
   test 'building a Build from Github payload' do
     Repository.delete_all
 
-    build = Build.create_from_github_payload(JSON.parse(GITHUB_PAYLOADS['gem-release']))
+    build = Build.create_from_github_payload(JSON.parse(GITHUB_PAYLOADS['gem-release'])).reload
 
+    assert_equal '1', build.number
     assert_equal '9854592', build.commit
     assert_equal 'Bump to 0.0.15', build.message
     assert_equal '2010-10-27 04:32:37 UTC', build.committed_at.to_formatted_s
@@ -19,5 +22,18 @@ class BuildTest < ActiveSupport::TestCase
     assert_equal 'svenfuchs@artweb-design.de', build.repository.owner_email
     assert_equal 'svenfuchs', build.repository.owner_name
     assert_equal 'http://github.com/svenfuchs/gem-release', build.repository.url
+  end
+
+  test 'next_number' do
+    repository = Factory(:repository)
+    assert_equal 1, repository.builds.next_number
+
+    repository = Factory(:repository)
+    3.times { |number| Factory(:build, :repository => repository, :number => number + 1) }
+    assert_equal 4, repository.builds.next_number
+
+    repository = Factory(:repository)
+    Factory(:build, :repository => repository, :number => '3.1')
+    assert_equal 4, repository.builds.next_number
   end
 end
