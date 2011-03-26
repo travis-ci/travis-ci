6a4cfd3a67ff1437fcb77f9c3254e6a4e6224917
Sven Fuchs <svenfuchs@artweb-design.de>
2011-03-26 19:56:51 +0100

    make Travis::Builder::Stdout buffer output for 2 secs before calling :on_log

--- lib/travis/builder/stdout.rb
+++ lib/travis/builder/stdout.rb
@@ -3,14 +3,37 @@ require 'em/stdout'
 module Travis
   class Builder
     module Stdout
-      attr_reader :stdout
+      class Buffer < String
+        def read
+          @read_pos ||= 0
+          string = self[@read_pos, length - @read_pos]
+          @read_pos += string.length
+          string
+        end
+      end
+
+      BUFFER_TIME = 2
+
+      attr_reader :stdout, :buffer
+
+      def initialize(*)
+        super
+        @buffer = Buffer.new
+      end
 
       def work!
         $_stdout = @stdout = EM.split_stdout do |c|
-          c.callback { |data| on_log(data) }
+          c.callback { |data| buffer << data }
+          c.on_close { flush }
         end
+        EventMachine.add_periodic_timer(BUFFER_TIME) { flush }
         super
+      end
+
+      def flush
+        on_log(buffer.read) unless buffer.empty?
       end
     end
   end
 end
+
