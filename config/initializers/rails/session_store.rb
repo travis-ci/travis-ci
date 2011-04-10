8ded6be4e7edefdfbd46ed32a22bdc25d2e9cb92
Erik Michaels-Ober <sferik@gmail.com>
2011-04-10 14:40:47 -0700

    Upgrade to Rails 3.0.6
    * Patch critical security flaws described in http://weblog.rubyonrails.org/2011/4/6/rails-3-0-6-has-been-released
    * Rename application to TravisCi to avoid namespace conflicts with Travis library and make future Rails upgrades easier

--- config/initializers/rails/session_store.rb
+++ config/initializers/rails/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Travis::Application.config.session_store :cookie_store, :key => '_travis_session'
+TravisCi::Application.config.session_store :cookie_store, :key => '_travis_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Travis::Application.config.session_store :active_record_store
+# TravisCi::Application.config.session_store :active_record_store
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Travis::Application.routes.draw do
+TravisCi::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   as :user do
