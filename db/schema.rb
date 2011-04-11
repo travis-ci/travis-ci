60f5db8a21a30ab69d2b43436eaaca7a3368d627
beawesomeinstead <graf.otodrakula@gmail.com>
2011-04-11 14:12:21 +0300

    Make search work with LIKEs.
    Looks like some of Heroku servers don't have full text indices
    support (untroduced in PostgreSQL 8.3).
    
    Use LIKEs unless we'll work this issue out.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110410141642) do
+ActiveRecord::Schema.define(:version => 20110321171101) do
 
   create_table "builds", :force => true do |t|
     t.integer  "repository_id"
