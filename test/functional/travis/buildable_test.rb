9424f5f5a181222553b4d9968f314d814b4dd8a4
Sven Fuchs <svenfuchs@artweb-design.de>
2011-04-15 17:51:37 +0200

    refactor worker system call to use bash -c and source the rvm script

--- test/functional/travis/buildable_test.rb
+++ test/functional/travis/buildable_test.rb
@@ -11,7 +11,7 @@ class BuildableTest < ActiveSupport::TestCase
     FileUtils.mkdir_p(Buildable.base_dir)
     Buildable.any_instance.stubs(:system)
     @stdout = $stdout
-    $stdout = StringIO.new
+    # $stdout = StringIO.new
   end
 
   def teardown
@@ -51,22 +51,22 @@ class BuildableTest < ActiveSupport::TestCase
   test "prepend_env: equals the build script if no config is given" do
     buildable = Buildable.new
     buildable.stubs(:config).returns(config)
-    assert_equal 'rake ci', buildable.prepend_env('rake ci')
+    assert_equal ['rake ci'], buildable.prepend_env('rake ci')
   end
 
   test "prepend_env: prepends an rvm command if configured" do
     buildable = Buildable.new(:config => { 'rvm' => '1.9.2' })
-    assert_equal %(rvm 1.9.2 exec 'rake ci'), buildable.prepend_env('rake ci')
+    assert_equal ['rvm use 1.9.2', 'rake ci'], buildable.prepend_env('rake ci')
   end
 
   test "prepend_env: prepends an env var if configured" do
     buildable = Buildable.new(:config => { 'gemfile' => 'gemfiles/rails-2.3.x' })
-    assert_equal %(BUNDLE_GEMFILE=gemfiles/rails-2.3.x rake ci), buildable.prepend_env('rake ci')
+    assert_equal ['BUNDLE_GEMFILE=gemfiles/rails-2.3.x rake ci'], buildable.prepend_env('rake ci')
   end
 
   test "prepend_env: prepends both rvm command and env var if configured" do
     buildable = Buildable.new(:config => { 'rvm' => '1.9.2', 'gemfile' => 'gemfiles/rails-2.3.x' })
-    assert_equal %(rvm 1.9.2 exec 'BUNDLE_GEMFILE=gemfiles/rails-2.3.x rake ci'), buildable.prepend_env('rake ci')
+    assert_equal ['rvm use 1.9.2', 'BUNDLE_GEMFILE=gemfiles/rails-2.3.x rake ci'], buildable.prepend_env('rake ci')
   end
 
   test 'build_dir: given a local filesystem url it returns a valid path' do
@@ -108,49 +108,57 @@ class BuildableTest < ActiveSupport::TestCase
 
   test 'install: runs bundle install' do
     buildable = Buildable.new(:config => { :not => :blank })
-    buildable.expects(:system).with('bundle install')
+    buildable.expects(:execute).with(['bundle install'])
     buildable.install
   end
 
   test 'install: runs bundle install w/ env vars prepended' do
     buildable = Buildable.new(:config => { 'env' => 'FOO=bar' })
-    buildable.expects(:system).with('FOO=bar bundle install')
+    buildable.expects(:execute).with(['FOO=bar bundle install'])
     buildable.install
   end
 
   test 'install: runs bundle install w/ rvm command prepended' do
     buildable = Buildable.new(:config => { 'rvm' => '1.8.7' })
-    buildable.expects(:system).with("rvm 1.8.7 exec 'bundle install'")
+    buildable.expects(:execute).with(['rvm use 1.8.7', 'bundle install'])
     buildable.install
   end
 
   test 'install: runs bundle install w/ rvm command and env vars prepended' do
     buildable = Buildable.new(:config => { 'script' => 'rake ci', 'rvm' => '1.8.7', 'env' => 'FOO=bar' })
-    buildable.expects(:system).with("rvm 1.8.7 exec 'FOO=bar bundle install'")
+    buildable.expects(:execute).with(['rvm use 1.8.7', 'FOO=bar bundle install'])
     buildable.install
   end
 
-  test 'execute: executes the build script' do
+  test 'run_script: executes the build script' do
     buildable = Buildable.new(:config => { 'script' => 'rake ci' })
-    buildable.expects(:system).with('rake ci')
-    buildable.execute
+    buildable.expects(:execute).with(['rake ci'])
+    buildable.run_script
   end
 
-  test 'execute: executes the build script w/ env vars prepended' do
+  test 'run_script: executes the build script w/ env vars prepended' do
     buildable = Buildable.new(:config => { 'script' => 'rake ci', 'env' => 'FOO=bar' })
-    buildable.expects(:system).with('FOO=bar rake ci')
-    buildable.execute
+    buildable.expects(:execute).with(['FOO=bar rake ci'])
+    buildable.run_script
   end
 
-  test 'execute: executes the build script w/ rvm command prepended' do
+  test 'run_script: executes the build script w/ rvm command prepended' do
     buildable = Buildable.new(:config => { 'script' => 'rake ci', 'rvm' => '1.8.7' })
-    buildable.expects(:system).with("rvm 1.8.7 exec 'rake ci'")
-    buildable.execute
+    buildable.expects(:execute).with(['rvm use 1.8.7', 'rake ci'])
+    buildable.run_script
   end
 
-  test 'execute: executes the build script w/ rvm command and env vars prepended' do
+  test 'run_script: executes the build script w/ rvm command and env vars prepended' do
     buildable = Buildable.new(:config => { 'script' => 'rake ci', 'rvm' => '1.8.7', 'env' => 'FOO=bar' })
-    buildable.expects(:system).with("rvm 1.8.7 exec 'FOO=bar rake ci'")
-    buildable.execute
+    buildable.expects(:execute).with(['rvm use 1.8.7', 'FOO=bar rake ci'])
+    buildable.run_script
+  end
+
+  test 'echoize: echo the command before executing it (1)' do
+    assert_equal "echo \\$\\ rake\nrake", Buildable.new.echoize('rake')
+  end
+
+  test 'echoize: echo the command before executing it (2)' do
+    assert_equal "echo \\$\\ rvm\\ use\\ 1.9.2\nrvm use 1.9.2\necho \\$\\ FOO\\=bar\\ rake\\ ci\nFOO=bar rake ci", Buildable.new.echoize(['rvm use 1.9.2', 'FOO=bar rake ci'])
   end
 end
