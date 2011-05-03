b9a157bc3bbda7c6bc98aaa0e8b48d7c9b0a96fa
charper <charper@charper.co.uk>
2011-05-03 15:36:45 +0100

    Sync data from github OAUTH payload on login

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,15 @@ class User < ActiveRecord::Base
   def self.find_for_github_oauth(user_hash)
     data = user_hash['extra']['user_hash']
     if user = User.find_by_login(data["login"])
+      user.update_attributes( User::user_data_from_github_data(data) )
       user
     else
-      create!(data.slice(*%w(name login email)))
+      create!(User::user_data_from_github_data(data))
     end
+  end
+
+  def self.user_data_from_github_data(data)
+      data.slice(*%w{name login email})
   end
 
   def profile_image_hash
@@ -25,4 +30,6 @@ class User < ActiveRecord::Base
     def create_a_token
       self.tokens.create!
     end
+
+
 end
