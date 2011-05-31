7ab333c1e2934afca0ad76931f2dd41bff8128a9
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2011-05-31 19:50:49 +0200

    Revert "Downgrading YAML to syck in order to fix 1.9.2 problem, please refer https://github.com/tenderlove/psych/issues/8 for details. "
    This reverts commit 553848e93d36e0b9f1fdfab91f34a1ab5dc5f118.

--- config/boot.rb
+++ config/boot.rb
@@ -1,15 +1,5 @@
 require 'rubygems'
 
-#
-# First of all, we need to switch to Syck b/c of ruby 1.9.2, which struggles with keys merge
-# please refer: https://github.com/tenderlove/psych/issues/8 and http://redmine.ruby-lang.org/issues/show/4300
-# for details.
-# If block is more b/c of ruby 1.8.7, since ENGINE was not introduced yet back then.
-#
-if YAML.const_defined?(:ENGINE)
-  YAML::ENGINE.yamler = 'syck'
-end
-
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
