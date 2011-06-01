e0d0a067828735fc5cdc691008f16724c1621d6b
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2011-06-01 19:15:39 +0200

    First build spec, more of a Proof of concept.

--- spec/acceptance/acceptance_helper.rb
+++ spec/acceptance/acceptance_helper.rb
@@ -3,3 +3,17 @@ require "steak"
 
 # Put your acceptance spec helpers inside /spec/acceptance/support
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+require "selenium-webdriver"
+
+RSpec.configure do |config|
+  config.before(:each) do
+    Capybara.current_driver = :selenium
+  end
+end
+
+Capybara.default_selector = :css
+
+Capybara.register_driver :selenium do |app|
+  Capybara::Driver::Selenium.new(app, { :browser => :chrome })
+end
+
---
+++ spec/acceptance/build_spec.rb
@@ -0,0 +1,16 @@
+require File.dirname(__FILE__) + '/acceptance_helper'
+
+
+feature "Feature name", %q{
+  As a non-registered user
+  I should see current build processes
+} do
+
+  scenario "build gets queued" do
+    visit "/"
+    Pusher['jobs'].trigger('build:queued', {"build" => {"id"=>9, "number"=>1}, "repository"=>{"id"=>3, :slug=>"rails/rails`" }})
+    wait_until do
+      find :xpath, "//*[contains(text(), 'rails/rails')]"
+    end
+  end
+end
