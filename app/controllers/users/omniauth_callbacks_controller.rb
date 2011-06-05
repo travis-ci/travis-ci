f1e61681178f4183bf412ce726d5e0f4ee5bf395
Sam Elliott <sam@lenary.co.uk>
2011-06-05 15:15:37 +0200

    This adds a welcome message for new users
    I was lost as to what to do with a project after I had signed up.
    Hopefully this will help people that come after me.
    
    The template lives in templates/tools because the pages break if it lives in templates/ and there wasn't a better place for it.
    
    It also breaks stuff for the github user called "welcome", but they have no repos so i don't care.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,7 +6,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "GitHub"
       sign_in @user, :event => :authentication
-      redirect_to root_url
+      if @user.created_at > 5.minutes.ago
+        # for some reason, if i use root_url(:anchor => "!/welcome") rails
+        # feels the need to escape the '/' thus breaking backbone.
+        redirect_to "/#!/welcome"
+      else
+        redirect_to root_url
+      end
     else
       redirect_to root_url
     end
