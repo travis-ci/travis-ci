98ef65cbff32ffbc82c0917379c4ead74b44658c
charper <charper@charper.co.uk>
2011-06-07 20:48:04 +0100

    Dont create a build if the repository is private

--- test/test_helpers/payloads.rb
+++ test/test_helpers/payloads.rb
@@ -1,4 +1,29 @@
 GITHUB_PAYLOADS = {
+  "private-repo" => %({
+    "repository": {
+      "url": "http://github.com/svenfuchs/gem-release",
+      "name": "gem-release",
+      "private":"true"
+      "owner": {
+        "email": "svenfuchs@artweb-design.de",
+        "name": "svenfuchs"
+      }
+    },
+    "commits": [{
+      "id":        "9854592",
+      "message":   "Bump to 0.0.15",
+      "timestamp": "2010-10-27 04:32:37",
+      "committer": {
+        "name":  "Sven Fuchs",
+        "email": "svenfuchs@artweb-design.de"
+      },
+      "author": {
+        "name":  "Christopher Floess",
+        "email": "chris@flooose.de"
+      }
+    }],
+    "ref": "refs/heads/master"
+  }),
   "gem-release" => %({
     "repository": {
       "url": "http://github.com/svenfuchs/gem-release",
