715ccc9cd4460fa277df12ba9e968eac08c6299f
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2011-06-09 19:24:23 +0200

    Added Devise initializator for controller methods that require authentication.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,4 +24,9 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  config.include Devise::TestHelpers, :type => :controller
 end
+
+
+
