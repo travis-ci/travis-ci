2eab6898a723a449e5c37778d9cfef58755dea9b
Sam Elliott <sam@lenary.co.uk>
2011-06-10 11:37:07 +0100

    Refactor user.rb to no longer rely on 5.minutes.ago

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,13 +6,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "GitHub"
       sign_in @user, :event => :authentication
-      if @user.created_at > 5.minutes.ago
-        # for some reason, if i use root_url(:anchor => "!/welcome") rails
-        # feels the need to escape the '/' thus breaking backbone.
-        redirect_to "/#!/welcome"
-      else
-        redirect_to root_url
-      end
+    end
+
+    if @user.recently_signed_up?
+      # for some reason, if i use root_url(:anchor => "!/welcome") rails
+      # feels the need to escape the '/' thus breaking backbone.
+      redirect_to "/#!/welcome"
     else
       redirect_to root_url
     end
