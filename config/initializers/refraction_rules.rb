c097c5b29c8612497dddf1f0912c9d54a9d1b9e2
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-06-12 01:30:01 +0200

    a small clean up of the refraction rules, no more /build passthrough

--- config/initializers/refraction_rules.rb
+++ config/initializers/refraction_rules.rb
@@ -3,14 +3,11 @@ require 'travis'
 Refraction.configure do |req|
 
   if req.host =~ /^#{Regexp.escape(Travis.config["heroku_domain"])}$/
-    if req.path =~ /^\/builds/
-      # passthrough with no change (old github webhook)
-      Rails.logger.add(1, "\n\nPASSTHROUGH : github webhook passthrough on old heroku address\n\n")
-    else
-      # old heroku address
-      Rails.logger.add(1, "\n\nREDIRECT : redirect issued for old heroku address\n\n")
-      req.permanent! :host => Travis.config["domain"]
-    end
+    # old heroku address
+    Rails.logger.add(1, "\n\nREDIRECT : redirect issued for heroku address\n\n")
+
+    req.permanent! :host => Travis.config["domain"]
+
     Rails.logger.flush if Rails.logger.respond_to?(:flush)
 
   elsif req.host =~ /([-\w]+\.)+#{Regexp.escape(Travis.config["domain"])}/
