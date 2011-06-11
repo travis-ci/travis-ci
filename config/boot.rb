dcc78caeddea2e499e23f2a578f2e1af57f0c1f5
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-06-11 23:45:26 +0200

    fixes an issue with the yaml engine and merging of keys

--- config/boot.rb
+++ config/boot.rb
@@ -3,4 +3,8 @@ require 'rubygems'
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
+if YAML.const_defined?("ENGINE")
+  YAML::ENGINE.yamler = 'syck'
+end
+
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
