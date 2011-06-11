52a29a2f56b5861d07179684793b210288ad2f67
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-06-12 00:51:37 +0200

    update the refraction rules to work with staging

--- config/initializers/refraction_rules.rb
+++ config/initializers/refraction_rules.rb
@@ -1,18 +1,24 @@
+require 'travis'
+
 Refraction.configure do |req|
-  if req.host =~ /travis\.heroku\.com/
+
+  if req.host =~ /^#{Regexp.escape(Travis.config["heroku_domain"])}$/
     if req.path =~ /^\/builds/
       # passthrough with no change (old github webhook)
       Rails.logger.add(1, "\n\nPASSTHROUGH : github webhook passthrough on old heroku address\n\n")
     else
       # old heroku address
       Rails.logger.add(1, "\n\nREDIRECT : redirect issued for old heroku address\n\n")
-      req.permanent! :host => "travis-ci.org"
+      req.permanent! :host => Travis.config["domain"]
     end
     Rails.logger.flush if Rails.logger.respond_to?(:flush)
-  elsif req.host =~ /([-\w]+\.)+travis-ci\.org/
+
+  elsif req.host =~ /([-\w]+\.)+\.#{Regexp.escape(Travis.config["domain"])}/
     # we don't want to use www for now
-    req.permanent! :host => "travis-ci.org"
+    req.permanent! :host => Travis.config["domain"]
+
   else
     # passthrough with no change
   end
+
 end
