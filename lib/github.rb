c4cf05a78d07c0fd151e94bff60e3d46067e868b
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2011-06-14 23:09:45 +0200

    Adding reminder about need to rewrite custom api calls to Octokit.

--- lib/github.rb
+++ lib/github.rb
@@ -2,6 +2,7 @@ require 'json'
 require 'ostruct'
 require 'core_ext/ostruct/hash_access'
 
+# TODO: we need to start using octokit everywhere by now. Or stick to that implementation, depending on team reaction.
 module Github
   module Api
     class << self
