ffb86f1bf0d78be989821d20ca873add6775b770
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2011-06-14 23:11:03 +0200

    Sign-in helper on top of Devise test helpers.

---
+++ spec/support/device_helper.rb
@@ -0,0 +1,15 @@
+module Devise
+  module SignInHelpers
+    # Creates a user form Factory and signs her in
+    def sign_in_new_user
+        @user = Factory.create(:user)
+        sign_in_user @user
+    end
+
+    # Signs in given user
+    def sign_in_user user
+        @request.env["devise.mapping"] = Devise.mappings[:user]
+        sign_in user
+    end
+  end
+end
