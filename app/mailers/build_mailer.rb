ed6bf8f9506c1c88c5c130b2bc36051913d88446
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-06-16 10:10:02 +0200

    The start of new and better build emails.

--- app/mailers/build_mailer.rb
+++ app/mailers/build_mailer.rb
@@ -1,6 +1,8 @@
 class BuildMailer < ActionMailer::Base
   default :from => 'notifications@travis-ci.org'
 
+  helper BuildMailerHelper
+
   def finished_email(build)
     @build     = build
     subject    = "#{build.repository.slug}##{build.number} (#{build.commit[0, 7]}): the build has #{build.passed? ? 'passed' : 'failed' }"
