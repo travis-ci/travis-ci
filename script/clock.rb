1f1a6352824b943b14c7d7b03fcdd19a1ab39519
Sven Fuchs <svenfuchs@artweb-design.de>
2011-06-17 17:54:55 +0200

    try disconnecting from redis in clock.rb

--- script/clock.rb
+++ script/clock.rb
@@ -6,6 +6,7 @@ require 'resque/heartbeat'
 handler do |job|
   Resque.redis = Travis.config['redis']['url']
   Resque.prune_dead_workers
+  Resque.redis.instance_variable_get(:@redis).client.disconnect
 end
 
 every 5.seconds, 'travis.purge_dead_workers'
