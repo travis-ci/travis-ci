11c6862d46763f05a3203251d0996ca417d58ec3
Sven Fuchs <svenfuchs@artweb-design.de>
2011-06-17 16:24:00 +0200

    try a clockwork background process on heroku for purging dead workers

---
+++ script/clock.rb
@@ -0,0 +1,10 @@
+require 'travis'
+require 'resque/heartbeat'
+
+handler do |job|
+  Resque.redis = Travis.config['redis']['url']
+  Resque.prune_dead_workers
+end
+
+every 5.seconds, 'travis.purge_dead_workers'
+
