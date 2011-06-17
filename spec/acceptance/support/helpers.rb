153de0e42d641b790919072970ddd23f02a5d652
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2011-06-17 11:17:50 +0200

    Adding mocks for in-browser Omniauth authentication without hitting github itself. Adding simple spec for my_repositories, just to assert its basic work.

--- spec/acceptance/support/helpers.rb
+++ spec/acceptance/support/helpers.rb
@@ -1,5 +1,35 @@
 module HelperMethods
-  # Put helper methods you need to be available in all tests here.
+  def should_see_text text
+    wait_until do
+      find :xpath, "//*[contains(text(), '#{text}')]"
+    end
+  end
+
+  def dispatch_pusher_command channel, command, params
+    page.evaluate_script("trigger('#{channel}', '#{command}', '#{params.to_json}' )")
+  end
+
+end
+
+module OmniauthHelperMethods
+  def mock_omniauth
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:github] = {
+      "provider" => "github",
+      "uid" => "12345",
+      'credentials' => {
+        'token' => 'access_token',
+        'secret' => 'secret'
+      },
+      'user_info' => {
+        'name'  => 'name',
+        'email' => 'email@gmail.com',
+        'nickname' => 'nickname',
+        'uid' => 'uid',
+      }
+    }
+  end
 end
 
 RSpec.configuration.include HelperMethods, :type => :acceptance
+RSpec.configuration.include OmniauthHelperMethods, :type => :acceptance
