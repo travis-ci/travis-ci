98bc1d1e55af9141adf9e38b7c719302f75138e0
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-06-19 21:07:09 +0200

    add missing remove_service_hook method to travis github api module

--- lib/travis/git_hub_api.rb
+++ lib/travis/git_hub_api.rb
@@ -19,6 +19,16 @@ module Travis
         raise ServiceHookError, 'error subscribing to the GitHub push event'
       end
 
+      # an error is thrown if there was a probem subscribing
+      def remove_service_hook(repository, user)
+        client = Octokit::Client.new(:oauth_token => user.github_oauth_token)
+
+        client.unsubscribe_service_hook(repository.owner_name, repository.name, "Travis")
+      rescue Octokit::UnprocessableEntity => e
+        # we might want to improve this for logging purposes
+        raise ServiceHookError, 'error unsubscribing from the GitHub push event'
+      end
+
       def repository_list_for_user(login)
         Octokit.repositories(login)
       end
