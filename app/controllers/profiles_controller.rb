fd5dedc4cf8a8125b1b7193c8dc90d8eedacaa5b
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-06-19 23:32:18 +0200

    some cleanups to the profile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,33 +1,40 @@
 class ProfilesController < ApplicationController
   before_filter :authenticate_user!
 
+  respond_to :json
+  respond_to :html, :only => :show
+
+  def show
+    @user = current_user
+
+    respond_with(@user)
+  end
+
   def service_hooks
     repositories = Repository.github_repos_for_user(current_user)
 
-    headers['Csrf-Token'] = form_authenticity_token
-    respond_to do |format|
-      format.json { render :json => repositories }
-    end
+    respond_with(repositories)
   end
 
   def add_service_hook
-    args = [params[:owner], params[:name], current_user]
+    repository = Repository.find_or_create_and_add_service_hook(*service_hook_args)
 
-    repository = Repository.find_or_create_and_add_service_hook(*args)
-
-    render :json => repository
+    respond_with(repository)
   rescue ActiveRecord::RecordInvalid, Travis::GitHubApi::ServiceHookError => e
-    render :json => repository, :status => :not_acceptable
+    respond_with(repository, :status => :not_acceptable)
   end
 
   def remove_service_hook
-    args = [params[:owner], params[:name], current_user]
+    repository = Repository.find_and_remove_service_hook(*service_hook_args)
 
-    repository = Repository.find_and_remove_service_hook(*args)
-
-    render :json => repository
+    respond_with(repository)
   rescue Travis::GitHubApi::ServiceHookError => e
-    render :json => @repository, :status => :not_acceptable
+    respond_with(repository, :status => :not_acceptable)
   end
 
+  private
+
+    def service_hook_args
+      [params[:owner], params[:name], current_user]
+    end
 end
