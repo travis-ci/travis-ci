5eaebd413a6ffec347985ad724e4ab7ff6656fbe
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-06-21 14:50:16 +0200

    tidyups of the two controller specs

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -4,75 +4,90 @@ require 'webmock/rspec'
 describe ProfilesController do
   include Devise::SignInHelpers
 
+  before(:each) do
+    Token.any_instance.stub(:token).and_return('afaketoken')
+    sign_in_user user
+  end
+  
+  let(:user)       { Factory(:user, :github_oauth_token => "myfaketoken") }
+  let(:repository) { Factory(:repository, :name => "minimal", :owner_name => "svenfuchs") }
+  
   describe "PUT 'update_service_hook'" do
-    before(:each) do
-      @user = Factory.create(:user, :github_oauth_token => "myfaketoken")
-      sign_in_user @user
-    end
-    let(:create_repository) { Factory.create(:repository, :name => "travis-ci", :owner_name => "sven")}
-    let(:stub_unsubscribe) {
-      stub_request(:post, "https://api.github.com/hub?access_token=myfaketoken").with(:body => {
-        'hub.mode' => "unsubscribe",
-        'hub.topic' => CGI.escape("https://github.com/sven/travis-ci/events/push"),
-        'hub.callback' => CGI.escape("github://Travis")
-      }.collect { |k,v| [ k,v ].join("=") }.join("&")).to_return(:status => 200, :body => "")
-    }
+    context "subscribes to Travis-CI service hook" do
+      before(:each) do
+        hub_body = {
+          'hub.mode' => "subscribe",
+          'hub.topic' => CGI.escape("https://github.com/svenfuchs/minimal/events/push"),
+          'hub.callback' => CGI.escape("github://Travis?token=afaketoken&user=svenfuchs&domain=")
+        }.collect { |k,v| [k,v].join("=") }.join("&")
+        
+        stub_request(:post, "https://api.github.com/hub?access_token=myfaketoken").
+          with(:body => hub_body).
+          to_return(:status => 200, :body => "")
+      end
+      
+      it "creates repository if it does not exist" do
+        put(:update_service_hook, :name => "minimal", :owner => "svenfuchs", :is_active => true)
 
-    let(:stub_subscribe) {
-      stub_request(:post, "https://api.github.com/hub?access_token=myfaketoken").with(:body => {
-        'hub.mode' => "subscribe",
-        'hub.topic' => CGI.escape("https://github.com/sven/travis-ci/events/push"),
-        'hub.callback' => CGI.escape("github://Travis?token=#{@user.tokens.first.token}&user=svenfuchs&domain=")
-      }.collect { |k,v| [ k,v ].join("=") }.join("&")).to_return(:status => 200, :body => "")
-    }
+        Repository.count.should eql(1)
+        Repository.first.is_active?.should eql(true)
+        
+        assert_requested(:post, "https://api.github.com/hub?access_token=myfaketoken", :times => 1)
+      end
 
-    it "should create repository if it does not exist" do
-      stub_subscribe
-      put :update_service_hook, :name => "travis-ci", :owner => "sven", :is_active => true
+      it "updates existing repository if it exists" do
+        repository # make sure the repo has been created
+        
+        put(:update_service_hook, :name => "minimal", :owner => "svenfuchs", :is_active => true)
 
-      Repository.count.should eql 1
-      Repository.find(:first).is_active.should eql true
-      assert_requested :post, "https://api.github.com/hub?access_token=myfaketoken", :times => 1
-    end
-
-    it "should update existing repository if it exists" do
-      stub_subscribe
-      create_repository
-      put :update_service_hook, :name => "travis-ci", :owner => "sven", :is_active => true
-
-      Repository.count.should eql 1
-      Repository.find(:first).is_active.should eql true
-      assert_requested :post, "https://api.github.com/hub?access_token=myfaketoken", :times => 1
+        Repository.count.should eql(1)
+        Repository.first.is_active?.should eql(true)
+        
+        assert_requested(:post, "https://api.github.com/hub?access_token=myfaketoken", :times => 1)
+      end
     end
 
-    it "should unsubscribe repository from travis-ci service" do
-      stub_unsubscribe
-      create_repository
+    context "unsubscribes from the Travis-CI service hook" do
+      before(:each) do
+        hub_body = {
+          'hub.mode' => "unsubscribe",
+          'hub.topic' => CGI.escape("https://github.com/svenfuchs/minimal/events/push"),
+          'hub.callback' => CGI.escape("github://Travis")
+        }.collect { |k,v| [ k,v ].join("=") }.join("&")
+        
+        stub_request(:post, "https://api.github.com/hub?access_token=myfaketoken").
+          with(:body => hub_body).
+          to_return(:status => 200, :body => "")
+      end
 
-      put :update_service_hook, :name => "travis-ci", :owner => "sven", :is_active => false
-      Repository.find(:first).is_active.should eql false
-      assert_requested :post, "https://api.github.com/hub?access_token=myfaketoken", :times => 1
+      it "updates existing repository" do
+        repository # make sure the repo has been created
+
+        put(:update_service_hook, :name => "minimal", :owner => "svenfuchs", :is_active => false)
+        
+        Repository.first.is_active?.should eql(false)
+        
+        assert_requested(:post, "https://api.github.com/hub?access_token=myfaketoken", :times => 1)
+      end
     end
   end
 
   describe "GET 'repositories'" do
-    before(:each) do
-      sign_in_new_user
-    end
-
     it "should return repositories of current user" do
-      stub_request(:get, "https://github.com/api/v2/json/repos/show/svenfuchs").to_return(:status => 200, :body => File.open("./spec/fixtures/github_user_repos.json").read)
-      get :service_hooks, :format => "json"
+      stub_request(:get, "https://github.com/api/v2/json/repos/show/svenfuchs").
+        to_return(:status => 200, :body => File.open("./spec/fixtures/github_user_repos.json").read)
+
+      get(:service_hooks, :format => "json")
 
       response.should be_success
 
       ## FIXME: probably it makes sense to verify these things agains a complete json, even though we care most about these fields
       result = ActiveSupport::JSON.decode response.body
 
-      result.first["name"].should eql "safemode"
-      result.first["owner"].should eql "svenfuchs"
-      result.second["name"].should eql "scriptaculous-sortabletree"
-      result.second["owner"].should eql "svenfuchs"
+      result.first["name"].should   eql("safemode")
+      result.first["owner"].should  eql("svenfuchs")
+      result.second["name"].should  eql("scriptaculous-sortabletree")
+      result.second["owner"].should eql("svenfuchs")
     end
   end
 
--- spec/controllers/repositories_controller_spec.rb
+++ spec/controllers/repositories_controller_spec.rb
@@ -2,31 +2,37 @@ require 'spec_helper'
 require 'webmock/rspec'
 
 describe RepositoriesController do
-  include Devise::SignInHelpers
 
   describe "GET 'index'" do
     before(:each) do
-      @repositories = [
-        Factory.create(:repository, :owner_name => "sven", :name => "travis-ci", :last_build_started_at => Date.today),
-        Factory.create(:repository, :owner_name => "josh", :name => "globalize", :last_build_started_at => Date.yesterday)]
+      # setup the two repos we need
+      Factory.create(:repository, :owner_name => "sven", :name => "travis-ci", :last_build_started_at => Date.today)
+      Factory.create(:repository, :owner_name => "josh", :name => "globalize", :last_build_started_at => Date.yesterday)
     end
+    
+    context "returns a list of repositories in json format" do
+      it "ordered by last build started date" do
+        get(:index, :format => :json)
+        
+        response.should be_success
+      
+        result = ActiveSupport::JSON.decode response.body
+      
+        result.count.should eql(2)
+        result.first["slug"].should  eql("sven/travis-ci")
+        result.second["slug"].should eql("josh/globalize")
+      end
 
-    it "should return list of repositories in json format, ordered by last build started date" do
-      get :index
-
-      response.should be_success
-      result = ActiveSupport::JSON.decode response.body
-      result.count.should eql 2
-      result.first["slug"].should eql "sven/travis-ci"
-      result.second["slug"].should eql "josh/globalize"
-    end
-
-    it "should return list of repositories in json format, filtered by owner name" do
-      get :index, :owner_name => "sven"
-      response.should be_success
-      result = ActiveSupport::JSON.decode response.body
-      result.count.should eql 1
-      result.first["slug"].should eql "sven/travis-ci"
+      it "filtered by owner name" do
+        get(:index, :owner_name => "sven", :format => :json)
+      
+        response.should be_success
+      
+        result = ActiveSupport::JSON.decode response.body
+      
+        result.count.should eql(1)
+        result.first["slug"].should eql("sven/travis-ci")
+      end
     end
   end
 
@@ -37,43 +43,49 @@ describe RepositoriesController do
 
     let(:repository) { Factory.create(:repository, :owner_name => "sven", :name => "fuchs") }
 
-    it 'should show an "unknown" button when the repository does not exist' do
-      controller.should_receive(:send_file).with("#{Rails.public_path}/images/status/unknown.png", {:type=>"image/png", :disposition=>"inline"}).once
+    it 'shows an "unknown" button when the repository does not exist' do
+      should_receive_file_with_status("unknown")
 
-      post :show, :format => "png", :owner_name => "sven", :name => "fuchs"
+      post(:show, :format => "png", :owner_name => "sven", :name => "fuchs")
     end
 
-    it 'should how an "unknown" button when it only has a build thats not finished' do
+    it 'shows an "unknown" button when it only has a build thats not finished' do
       Factory(:running_build)
 
-      controller.should_receive(:send_file).with("#{Rails.public_path}/images/status/unknown.png", {:type=>"image/png", :disposition=>"inline"}).once
+      should_receive_file_with_status("unknown")
 
-      post :show, :format => "png", :owner_name => "sven", :name => "fuchs"
+      post(:show, :format => "png", :owner_name => "sven", :name => "fuchs")
     end
 
-    it 'should show an "unstable" button when the repository has broken build' do
+    it 'shows an "unstable" button when the repository has broken build' do
       Factory(:broken_build, :repository => repository)
 
-      controller.should_receive(:send_file).with("#{Rails.public_path}/images/status/unstable.png", {:type=>"image/png", :disposition=>"inline"}).once
+      should_receive_file_with_status("unstable")
 
-      post :show, :format => "png", :owner_name => "sven", :name => "fuchs"
+      post(:show, :format => "png", :owner_name => "sven", :name => "fuchs")
     end
 
-    it 'should show a "stable" button when the repository\'s last build passed' do
+    it 'shows a "stable" button when the repository\'s last build passed' do
       Factory(:successfull_build, :repository => repository)
 
-      controller.should_receive(:send_file).with("#{Rails.public_path}/images/status/stable.png", {:type=>"image/png", :disposition=>"inline"}).once
+      should_receive_file_with_status("stable")
 
-      post :show, :format => "png", :owner_name => "sven", :name => "fuchs"
+      post(:show, :format => "png", :owner_name => "sven", :name => "fuchs")
     end
 
-    it 'should show a "stable" button when the previous build passed and there\'s one still running' do
+    it 'shows a "stable" button when the previous build passed and there\'s one still running' do
       Factory(:successfull_build, :repository => repository)
       Factory(:running_build, :repository => repository)
 
-      controller.should_receive(:send_file).with("#{Rails.public_path}/images/status/stable.png", {:type=>"image/png", :disposition=>"inline"}).once
+      should_receive_file_with_status("stable")
 
-      post :show, :format => "png", :owner_name => "sven", :name => "fuchs"
+      post(:show, :format => "png", :owner_name => "sven", :name => "fuchs")
+    end
+    
+    def should_receive_file_with_status(status)
+      controller.should_receive(:send_file).
+        with("#{Rails.public_path}/images/status/#{status}.png", { :type=>"image/png", :disposition=>"inline" }).
+        once
     end
   end
 end
