18eda7a8088f6ddbc398e650f5136560faae3392
Sven Fuchs <svenfuchs@artweb-design.de>
2011-07-07 01:17:32 +0200

    list jobs for :builds and :rails queues (needs improvement! rails jobs will always be listed last, we should display a list per queue)

--- lib/resque_helpers.rb
+++ lib/resque_helpers.rb
@@ -2,12 +2,14 @@ module ResqueHelpers
 
   class << self
     def queued_jobs
-      Resque.peek(:builds, 0, 50).map do |job|
-        job['args'].last.tap do |data|
-          data['repository'].slice!('id', 'slug')
-          data.update(data['build'].slice('id', 'number', 'commit'))
-          data.delete('build')
-        end
+      [:builds, :rails].each do |queue|
+        Resque.peek(queue, 0, 50).map do |job|
+          job['args'].last.tap do |data|
+            data['repository'].slice!('id', 'slug')
+            data.update(data['build'].slice('id', 'number', 'commit'))
+            data.delete('build')
+          end
+        end.flatten
       end
     end
 
