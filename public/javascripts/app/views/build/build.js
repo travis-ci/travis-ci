44a3e0b3ed9ee8e34980420c8010a08622507eae
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2011-07-11 17:42:56 +0200

    Moving line number activation from build to log, since it belongs there. Refactoring & removing links to parent / parent. If we have shared params, let them be environment params.

--- public/javascripts/app/views/build/build.js
+++ public/javascripts/app/views/build/build.js
@@ -63,8 +63,6 @@ Travis.Views.Build.Build = Backbone.View.extend({
           this._renderMatrix();
       } else {
         this._renderLog();
-        this._initializeEvents();
-        this._activateCurrentLine();
       }
     }
   },
@@ -72,33 +70,10 @@ Travis.Views.Build.Build = Backbone.View.extend({
     this.el.append(new Travis.Views.Build.Summary({ model: this.build, parent: this }).render().el);
   },
   _renderLog: function() {
-    this.el.append(new Travis.Views.Build.Log({ model: this.build, parent: this }).render().el);
-  },
-  _initializeEvents: function() {
-    var self = this
-    _.each(this.el.find('p.line'), function(el) {
-      $(el).click(function(){
-        var e = self.parent.parent.parent.path_elements;
-        // TODO: CREATE PATH HELPERS??
-        // Why haven't I done it through anchor + tag? B/c i hate element like that:
-        //     <a href="/#!/josevalim/enginex/L31" name="#!/josevalim/enginex/L31">LINE CONTENTS</a>.
-        // I think it's easier to create event once here than do it through native anchors.
-        window.location.href = [ "#!/", e.owner, "/", e.name, "/L", $(el).attr('name').replace('line', '') ].join('')
-      })
-    })
-  },
-  _activateCurrentLine: function() {
-    if(this.getLogLineNumber()) {
-      var line_element = this.el.find("p[name='line" + this.getLogLineNumber()  + "']")
-      $(window).scrollTop(line_element.offset().top)
-      line_element.addClass("highlight")
-    }
-  },
-  getLogLineNumber: function () {
-    if(this.parent && this.parent.parent)
-      return this.parent.parent.parent.path_elements.line_number;
-    else
-      return null;
+    this.log = new Travis.Views.Build.Log({ model: this.build, parent: this })
+    this.el.append(this.log.render().el);
+    this.log.initializeEvents();
+    this.log.activateCurrentLine();
   },
   _renderMatrix: function() {
     this.el.append(new Travis.Views.Build.Matrix.Table({ builds: this.build.matrix }).render().el);
--- public/javascripts/app/views/build/log.js
+++ public/javascripts/app/views/build/log.js
@@ -1,6 +1,7 @@
 Travis.Views.Build.Log = Backbone.View.extend({
   initialize: function() {
     _.bindAll(this, 'render', 'setLog', 'appendLog');
+    _.extend(this, this.options);
     this.template = Travis.templates['build/log'];
     if(this.model) this.attachTo(this.model);
   },
@@ -20,6 +21,26 @@ Travis.Views.Build.Log = Backbone.View.extend({
   render: function() {
     this.el = $(this.template({ log: Utils.filterLog(this.model.get('log') || '') }));
     return this;
+  },
+  initializeEvents: function() {
+    var self = this
+    _.each(this.el.find('p.line'), function(el) {
+      $(el).click(function(){
+        var e = window.params;
+        // TODO: CREATE PATH HELPERS??
+        // Why haven't I done it through anchor + tag? B/c i hate element like that:
+        //     <a href="/#!/josevalim/enginex/L31" name="#!/josevalim/enginex/L31">LINE CONTENTS</a>.
+        // I think it's easier to create event once here than do it through native anchors.
+        window.location.href = [ "#!/", e.owner, "/", e.name, "/L", $(el).attr('name').replace('line', '') ].join('')
+      })
+    })
+  },
+  activateCurrentLine: function() {
+    if(window.params.line_number) {
+      var line_element = this.el.find("p[name='line" + window.params.line_number  + "']")
+      $(window).scrollTop(line_element.offset().top)
+      line_element.addClass("highlight")
+    }
   },
   setLog: function() {
     this.el.html(Utils.filterLog(this.model.get('log') || ''));
