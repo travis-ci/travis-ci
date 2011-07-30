e2db75f5f5d51e6e2d7086ac77fe2322af06fb65
Sven Fuchs <svenfuchs@artweb-design.de>
2011-07-30 06:49:09 +0200

    refactor webhook json

--- app/models/build/json.rb
+++ app/models/build/json.rb
@@ -12,12 +12,23 @@ class Build
       :'build:started'    => all_attrs - [:status, :log, :finished_at],
       :'build:configured' => all_attrs - [:status, :log, :finished_at],
       :'build:log'        => [:id, :parent_id],
-      :'build:finished'   => [:id, :parent_id, :status, :finished_at]
+      :'build:finished'   => [:id, :parent_id, :status, :finished_at],
+      :webhook            => [:id, :build_log_url, :number, :commit, :branch, :message, :status, :started_at, :finished_at,
+                              :committed_at, :committer_name, :committer_email, :author_name, :author_email, :compare_url]
+    }
+
+    JSON_MERGE = {
+      :webhook => lambda {|build| {
+        :repository => build.repository.as_json(:for => :webhook),
+        :github_url => build.repository.url, # TODO shouldn't this be part of the repository?
+        :status_message => build.status_message
+      } }
     }
 
     def as_json(options = nil)
       options ||= {}
       json = super(:only => JSON_ATTRS[options[:for] || :default])
+      json.merge!(JSON_MERGE[options[:for]].call(self)) if JSON_MERGE[options[:for]]
       json.merge!('matrix' => matrix.as_json(:for => options[:for])) if matrix?
       json.compact
     end
