8e49a4fe99d923f5598a525f5868aacbddfcfb95
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-08-01 18:26:47 +0200

    Don't raise RoutingErrors when the format is .png

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -8,10 +8,11 @@ class RepositoriesController < ApplicationController
   end
 
   def show
-    not_found unless @repository = repository
+    @repository = repository
 
     respond_with(@repository) do |format|
       format.png { send_status_image_file }
+      format.any { @repository || not_found }
     end
   end
 
