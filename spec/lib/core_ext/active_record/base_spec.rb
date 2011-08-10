3c422ac7d67742cf9a58fc92ced0dc53a9ce8c08
Sven Fuchs <svenfuchs@artweb-design.de>
2011-08-10 04:31:25 +0200

    implement active_record/base spec

--- spec/lib/core_ext/active_record/base_spec.rb
+++ spec/lib/core_ext/active_record/base_spec.rb
@@ -3,8 +3,28 @@ require 'core_ext/active_record/base'
 
 describe ActiveRecord::Base, 'extensions' do
   describe 'floor' do
-    xit 'returns an sql snippet for postgres'
-    xit 'returns an sql snippet for mysql'
-    xit 'returns an sql snippet for sqlite3'
+    subject { ActiveRecord::Base }
+
+    before(:each) { @adapter = subject.configurations['test']['adapter'] }
+    after(:each)  { subject.configurations['test']['adapter'] = @adapter }
+
+    def adapter(adapter)
+      subject.configurations['test']['adapter'] = adapter
+    end
+
+    it 'returns an sql snippet for postgres' do
+      adapter 'postgresql'
+      subject.floor(:number).should == 'floor(number::float)'
+    end
+
+    it 'returns an sql snippet for mysql' do
+      adapter 'mysql'
+      subject.floor(:number).should == 'floor(number)'
+    end
+
+    it 'returns an sql snippet for sqlite3' do
+      adapter 'sqlite3'
+      subject.floor(:number).should == 'round(number - 0.5)'
+    end
   end
 end
