a46dd02710e9251558c51ff7336ca0d6ec6c482b
Joost Baaij <joost@spacebabies.nl>
2011-08-22 20:47:16 +0200

    introduce test for BuildMailer

---
+++ spec/mailers/build_mailer_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe BuildMailer do
+  describe 'finished_email' do
+    let(:build) { Factory(:successfull_build) }
+    let(:mail)  { BuildMailer.finished_email(build) }
+
+    it 'subject' do
+      mail.subject.should == "svenfuchs/successfull_build#1 (master - 62aae5f): the build has passed"
+    end
+
+    it 'recipient' do
+      mail.to.should == ["svenfuchs@artweb-design.de"]
+    end
+
+    it 'sender' do
+      mail.from.should == ["notifications@travis-ci.org"]
+    end
+
+    it 'displays the build number' do
+      mail.body.encoded.should include(build.number.to_s)
+    end
+
+    it 'displays the status message' do
+      mail.body.encoded.should include(build.status_message)
+    end
+  end
+end
