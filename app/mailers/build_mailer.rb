c5f53241aeefdf5201a5a4094ad52879cc16ba06
Joost Baaij <joost@spacebabies.nl>
2011-08-22 21:36:16 +0200

    variable to method
    Conflicts:
    
    	app/mailers/build_mailer.rb

--- app/mailers/build_mailer.rb
+++ app/mailers/build_mailer.rb
@@ -6,8 +6,12 @@ class BuildMailer < ActionMailer::Base
   def finished_email(build)
     @build     = build
     @commit    = build.commit
-    subject    = "#{build.repository.slug}##{build.number} (#{@commit.branch} - #{@commit.commit[0, 7]}): the build has #{build.passed? ? 'passed' : 'failed' }"
     recipients = build.email_recipients
     mail(:to => recipients, :subject => subject)
   end
+  
+  private
+    def subject
+      "#{@build.repository.slug}##{@build.number} (#{@commit.branch} - #{@commit.commit[0, 7]}): the build has #{@build.passed? ? 'passed' : 'failed' }"
+    end
 end
