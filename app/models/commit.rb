96b88735b4cd2035d5f707b1e1a55e45e452a848
Sven Fuchs <svenfuchs@artweb-design.de>
2011-08-25 01:32:15 +0200

    it seems committer_[name|email] aren't always set

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -1,5 +1,4 @@
 class Commit < ActiveRecord::Base
   belongs_to :repository
-  validates :commit, :branch, :message, :committed_at, :committer_name, :committer_email, :presence => true
+  validates :commit, :branch, :message, :committed_at, :presence => true
 end
-
