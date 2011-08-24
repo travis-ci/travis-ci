cf3e63ae0677113b288779b8333cb3d6af9a99ce
Damien Mathieu <42@dmathieu.com>
2011-08-24 11:21:29 +0200

    allow dots as owner_name and repository name - Closes #192 Conflicts:
    	config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,16 @@ require 'patches/rails_route_set'
 TravisCi::Application.routes.draw do
   root :to => 'home#index'
 
-  match ":owner_name/:name.png", :to => 'repositories#show', :format => 'png'
-  match ":owner_name/:name.json", :to => 'repositories#show', :format => 'json'
-  match ":owner_name/:name.xml", :to => 'repositories#show', :format => 'xml'
-  match ":owner_name/:name/cc.xml", :to => 'repositories#show', :format => 'xml', :schema => 'cctray'
+  match "*owner_name/*name.png", :to => 'repositories#show', :format => 'png'
+  match "*owner_name/*name.json", :to => 'repositories#show', :format => 'json'
+  match "*owner_name/*name.xml", :to => 'repositories#show', :format => 'xml'
+  match "*owner_name/*name/cc.xml", :to => 'repositories#show', :format => 'xml', :schema => 'cctray'
 
-  match ":owner_name/:name/builds.xml", :to => 'builds#index', :format => 'xml'
-  match ":owner_name/:name/builds.json", :to => 'builds#index', :format => 'json'
-  match ":owner_name/:name/builds/:id.xml", :to => 'builds#show', :format => 'xml'
-  match ":owner_name/:name/builds/:id.json", :to => 'builds#show', :format => 'json'
+  match "*owner_name/*name/builds.xml", :to => 'builds#index', :format => 'xml'
+  match "*owner_name/*name/builds.json", :to => 'builds#index', :format => 'json'
+
+  match "*owner_name/*name/builds/:id.xml", :to => 'builds#show', :format => 'xml'
+  match "*owner_name/*name/builds/:id.json", :to => 'builds#show', :format => 'json'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
