8479c556392f2d1de844f5b0695a6671dcc7fa99
Sven Fuchs <svenfuchs@artweb-design.de>
2011-08-28 02:48:46 +0200

    remove unused methods from the profiles_controller (they've been in the service_hooks_controller for quite a while)

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,32 +7,12 @@ class ProfilesController < ApplicationController
   respond_to :html, :only => :show
 
   def show
-    @user = current_user
-
-    respond_with(@user)
-  end
-
-  def service_hooks
-    repositories = Repository.github_repos_for_user(current_user)
-
-    respond_with(repositories)
-  end
-
-  def update_service_hook
-    if params[:active]
-      repository = Repository.find_or_create_and_add_service_hook(*service_hook_args)
-    else
-      repository = Repository.find_and_remove_service_hook(*service_hook_args)
-    end
-
-    respond_with(repository)
-  rescue ActiveRecord::RecordInvalid, Travis::GithubApi::ServiceHookError => e
-    respond_with(repository, :only => [:id, :active], :status => :not_acceptable)
+    respond_with(user)
   end
 
   private
 
-    def service_hook_args
-      [params[:owner], params[:name], current_user]
+    def user
+      @user ||= current_user
     end
 end
