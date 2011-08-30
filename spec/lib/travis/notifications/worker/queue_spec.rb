b61e599763e08ecb6f6479a8aad4bb5c1d43d52b
Sven Fuchs <svenfuchs@artweb-design.de>
2011-08-30 13:39:16 +0200

    port worker routing based on :language from backup.production

--- spec/lib/travis/notifications/worker/queue_spec.rb
+++ spec/lib/travis/notifications/worker/queue_spec.rb
@@ -5,8 +5,9 @@ describe Travis::Notifications::Worker::Queue do
     Travis::Notifications::Worker::Queue.new(*args)
   end
 
-  let(:rails)  { queue('rails', 'rails/rails') }
-  let(:erlang) { queue('rails', nil, 'erlang') }
+  let(:rails)   { queue('rails', 'rails/rails', nil) }
+  let(:erlang)  { queue('erlang', nil, 'erlang', nil) }
+  let(:clojure) { queue('builds', nil, nil, 'clojure') }
 
   it "to_s returns Travis::Worker" do
     rails.to_s.should == "Travis::Worker"
@@ -18,15 +19,19 @@ describe Travis::Notifications::Worker::Queue do
 
   describe 'matches?' do
     it "returns true when the given slug matches" do
-      rails.matches?('rails/rails', nil).should be_true
+      rails.matches?('rails/rails', nil, nil).should be_true
     end
 
-    it "returns true when the given slug matches" do
-      rails.matches?('rails/rails', nil).should be_true
+    it "returns true when the given target matches" do
+      erlang.matches?(nil, 'erlang', nil).should be_true
     end
 
-    it "returns false when the given target matches" do
-      erlang.matches?('foo/bar', 'COBOL').should be_false
+    it "returns true when the given language matches" do
+      clojure.matches?(nil, nil, 'clojure').should be_true
+    end
+
+    it "returns false when none of slug, target or language match" do
+      erlang.matches?('foo/bar', 'worker-on-mars', 'COBOL').should be_false
     end
   end
 end
