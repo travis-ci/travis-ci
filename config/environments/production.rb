eae5f957ec2c7f54d24fe5303f0491af592a131a
Sven Fuchs <svenfuchs@artweb-design.de>
2011-08-31 17:33:14 +0200

    try setting config.assets.compile to true for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,8 @@ TravisCi::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
