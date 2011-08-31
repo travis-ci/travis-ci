341ec04b7177433d9e73c54c4497f2d68e096ee3
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-08-31 02:38:06 -0500

    Rails 3.1: Setting new defaults, using new gems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,8 +32,15 @@ TravisCi::Application.configure do
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
   # config.serve_static_assets = true
-  config.assets.js_compressor  = :uglifier
-  config.assets.css_compressor = :scss
+
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = 'http://assets.example.com'
