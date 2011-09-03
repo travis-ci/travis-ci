e3367a6bdc3137251f2f7b93b21be2477e122fa2
Sven Fuchs <svenfuchs@artweb-design.de>
2011-09-03 16:23:37 +0200

    yay, got jasmine specs running in both the browser (`rails s -e jasmine`) and jasmine-headless-webkit

---
+++ app/assets/javascripts/spec/support/jasmine_reporter.js
@@ -0,0 +1,19 @@
+var jsApiReporter;
+(function() {
+  var jasmineEnv = jasmine.getEnv();
+
+  jsApiReporter = new jasmine.JsApiReporter();
+  var trivialReporter = new jasmine.TrivialReporter();
+
+  jasmineEnv.addReporter(jsApiReporter);
+  jasmineEnv.addReporter(trivialReporter);
+
+  jasmineEnv.specFilter = function(spec) {
+    return trivialReporter.specFilter(spec);
+  };
+
+  window.onload = function() {
+    jasmineEnv.execute();
+    $('body').append($('<div id="jasmine_content"></div>'));
+  };
+})();
--- app/assets/javascripts/specs.js
+++ app/assets/javascripts/specs.js
@@ -1,4 +1,6 @@
 //= require jasmine
+//= require jasmine-html
 //= require_tree ./spec/vendor
 //= require      ./spec/spec_helper.js
 //= require_tree ./spec/support
+//= require_tree ./spec
