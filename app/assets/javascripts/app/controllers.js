2d2b25cb5d9dc5aa3fdb9624625edc55463613ef
Sven Fuchs <svenfuchs@artweb-design.de>
2011-09-05 21:07:51 +0200

    allow repositories.json?slug=foo/bar for now

--- app/assets/javascripts/app/controllers.js
+++ app/assets/javascripts/app/controllers.js
@@ -20,16 +20,16 @@ Travis.Controllers = {
       var slug = $.compact([this.getPath('params.owner'), this.getPath('params.name')]).join('/');
       return slug.length > 0 ? Travis.Repository.bySlug(slug) : Travis.Repository.latest();
     }.property('params'),
+
+    tabs: SC.Object.create({
+      current: SC.Object.create({
+        summaryCssClasses: function() {
+          return $.compact(['summary', 'clearfix', this.getPath('build.color')]).join(' ');
+        }.property('build')
+      }),
+
+      builds: SC.Object.create({
+      })
+    })
   }),
-
-  // tabs: SC.Object.create({
-  //   current: SC.Object.create({
-  //     summaryCssClasses: function() {
-  //       return $.compact(['summary', 'clearfix', this.getPath('build.color')]).join(' ');
-  //     }.property('build')
-  //   }),
-
-  //   builds: SC.Object.create({
-  //   })
-  // })
 };
