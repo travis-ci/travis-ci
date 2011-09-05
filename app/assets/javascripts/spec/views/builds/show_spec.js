75d4375c573214a1db8e690468eeb4c1a381e4b1
Sven Fuchs <svenfuchs@artweb-design.de>
2011-09-05 05:28:42 +0200

    shuffle templates around subdirs

---
+++ app/assets/javascripts/spec/views/builds/show_spec.js
@@ -0,0 +1,105 @@
+describe('Views:', function() {
+  describe('builds', function() {
+    describe('show', function() {
+      var tabs, build, view;
+
+      beforeEach(function() {
+        $('#main').empty();
+
+        tabs = Travis.Controllers.tabs;
+        tabs.active = tabs.current;
+        build = Test.Factory.Build.passing();
+        view = SC.View.create({ tab: Travis.Controllers.tabs.active, template: SC.TEMPLATES['app/templates/builds/show'] });
+
+        SC.run(function() { view.appendTo('#main'); });
+        SC.run(function() { tabs.active.set('build', build); });
+      });
+
+      afterEach(function() {
+        view.destroy();
+      });
+
+      it('shows the current build', function() {
+        expect(view.$()).toShowBuildSummary(build);
+      });
+
+      describe('when a build property changes', function() {
+        it('updates the number', function() {
+          SC.run(function() { build.set('number', '111'); });
+          expect(view.$()).toShowBuildSummary(build);
+        });
+
+        it('updates the duration and finished_at time', function() {
+          SC.run(function() { build.set('finishedAt', '2011-01-01T03:00:20Z'); });
+          expect(view.$()).toShowBuildSummary(build);
+        });
+
+        it('updates the commit', function() {
+          SC.run(function() { build.set('commit', 'abcdefgh'); });
+          expect(view.$()).toShowBuildSummary(build);
+        });
+
+        it('updates the branch', function() {
+          SC.run(function() { build.set('branch', 'feature-ponies'); });
+          expect(view.$()).toShowBuildSummary(build);
+        });
+
+        it('updates the author name', function() {
+          SC.run(function() { build.set('authorName', 'Yukihiro Matsumoto'); });
+          expect(view.$()).toShowBuildSummary(build);
+        });
+
+        it('updates the author email', function() {
+          SC.run(function() { build.set('authorEmail', 'matz@ruby-lang.org'); });
+          expect(view.$()).toShowBuildSummary(build);
+        });
+
+        it('updates the committer name', function() {
+          SC.run(function() { build.set('committerName', 'Yukihiro Matsumoto'); });
+          expect(view.$()).toShowBuildSummary(build);
+        });
+
+        it('updates the committer email', function() {
+          SC.run(function() { build.set('committerEmail', 'matz@ruby-lang.org'); });
+          expect(view.$()).toShowBuildSummary(build);
+        });
+
+        it('updates the message', function() {
+          SC.run(function() { build.set('message', 'OMG PONIES!'); });
+          expect(view.$()).toShowBuildSummary(build);
+        });
+
+        it('updates the config', function() {
+          SC.run(function() { build.set('config', { rvm: ['rbx'], gemfile: ['Gemfile.ponies'] }); });
+          expect(view.$()).toShowBuildSummary(build);
+        });
+      });
+
+      describe('with a multi-build matrix', function() {
+        it('renders the matrix view', function() {
+          SC.run(function() {
+            var attributes = build.get('matrix').objectAt(0).get('attributes');
+            Travis.store.loadRecord(Travis.Build, $.merge(attributes, { id: 111, parent_id: build.get('id') }));
+            // build.get('matrix').pushObject(Travis.store.find(Travis.Build, 111));
+          });
+          // TODO can't get the matrix to update here :/
+        });
+      });
+
+      describe('with a single-build matrix', function() {
+        it('renders the log', function() {
+          SC.run(function() { tabs.active.set('build', build); });
+          expect(view.$()).toContain('pre.log')
+        });
+      });
+
+      describe('with a test', function() {
+        it('renders the log', function() {
+          SC.run(function() { tabs.active.set('build', build.get('matrix').get('firstObject')); });
+          expect(view.$()).toContain('pre.log')
+          expect(view.$('pre.log')).toHaveText('Done. Build script exited with: 0')
+        });
+      });
+    });
+  });
+});
---
+++ app/assets/javascripts/spec/views/repositories/show_spec.js
@@ -0,0 +1,47 @@
+describe('Views:', function() {
+  describe('repositories', function() {
+    describe('show', function() {
+      var repository, view;
+
+      beforeEach(function() {
+        $('#main').empty();
+
+        repository = Test.Factory.Repository.travis();
+        view = SC.View.create({ template: SC.TEMPLATES['app/templates/repositories/show'] });
+
+        SC.run(function() { view.appendTo('#main'); });
+        SC.run(function() { Travis.Controllers.repository.set('content', repository); });
+      });
+
+      afterEach(function() {
+        view.destroy();
+      });
+
+      it('shows the current repository', function() {
+        expect(view.$()).toShowRepository(repository);
+      });
+
+      describe('when a repository property changes', function() {
+        it('updates the slug', function() {
+          SC.run(function() { repository.set('slug', 'updated/slug'); });
+          expect(view.$()).toShowRepository(repository);
+        });
+
+        it('updates the last build number', function() {
+          SC.run(function() { repository.set('lastBuildNumber', '111'); });
+          expect(view.$()).toShowRepository(repository);
+        });
+
+        it('updates the last build duration and last build finished_at time', function() {
+          SC.run(function() { repository.set('lastBuildFinishedAt', '2011-01-01T03:00:20Z'); });
+          expect(view.$()).toShowRepository(repository);
+        });
+
+        it('updates the last build url', function() {
+          SC.run(function() { repository.set('lastBuildId', 111); });
+          expect(view.$()).toShowRepository(repository);
+        });
+      });
+    });
+  });
+});
