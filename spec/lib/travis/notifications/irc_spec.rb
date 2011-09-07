49616def5df8a7cc58bf8ddf066d7b86da8077d2
Nathan Broadbent <nathan.f77@gmail.com>
2011-09-07 14:42:07 +0800

    IRC notifications spec for two notifications only tested for the presence of one notification. Added a spec to ensure that configuring notification rules also works here.

--- spec/lib/travis/notifications/irc_spec.rb
+++ spec/lib/travis/notifications/irc_spec.rb
@@ -58,13 +58,46 @@ describe Travis::Notifications::Irc do
       '[travis-ci] svenfuchs/minimal#1 (master - 62aae5f : Sven Fuchs): the build has failed',
       '[travis-ci] Change view : https://github.com/svenfuchs/minimal/compare/master...develop',
       "[travis-ci] Build details : http://test.travis-ci.org/svenfuchs/minimal/builds/#{build.id}",
+      "PART #travis",
+      "QUIT",
       'JOIN #rails',
       '[travis-ci] svenfuchs/minimal#1 (master - 62aae5f : Sven Fuchs): the build has failed',
       '[travis-ci] Change view : https://github.com/svenfuchs/minimal/compare/master...develop',
       "[travis-ci] Build details : http://test.travis-ci.org/svenfuchs/minimal/builds/#{build.id}",
     ]
 
-    0.upto(3).each { |ix|
+    0.upto(7).each { |ix|
+      irc.output[ix].should == expected[ix]
+    }
+  end
+
+  it "two irc notifications with notification rules" do
+    build = Factory(:build, :config => {
+                              'notifications' => {
+                                'irc' => {
+                                  'on_success' => "always",
+                                  'channels' =>
+                                      ["irc.freenode.net:1234#travis", "irc.freenode.net#rails"]}}})
+
+    expect_irc('irc.freenode.net', { :port => '1234' })
+    expect_irc('irc.freenode.net')
+
+    Travis::Notifications::Irc.new.notify('build:finished', build)
+
+    expected = [
+      'JOIN #travis',
+      '[travis-ci] svenfuchs/minimal#1 (master - 62aae5f : Sven Fuchs): the build has failed',
+      '[travis-ci] Change view : https://github.com/svenfuchs/minimal/compare/master...develop',
+      "[travis-ci] Build details : http://test.travis-ci.org/svenfuchs/minimal/builds/#{build.id}",
+      "PART #travis",
+      "QUIT",
+      'JOIN #rails',
+      '[travis-ci] svenfuchs/minimal#1 (master - 62aae5f : Sven Fuchs): the build has failed',
+      '[travis-ci] Change view : https://github.com/svenfuchs/minimal/compare/master...develop',
+      "[travis-ci] Build details : http://test.travis-ci.org/svenfuchs/minimal/builds/#{build.id}",
+    ]
+
+    0.upto(7).each { |ix|
       irc.output[ix].should == expected[ix]
     }
   end
