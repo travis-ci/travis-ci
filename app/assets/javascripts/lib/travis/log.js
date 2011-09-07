939a56f2b768a088364cac7c79431caebf6ed578
Sven Fuchs <svenfuchs@artweb-design.de>
2011-09-08 00:31:28 +0200

    fix log folding

--- app/assets/javascripts/lib/travis/log.js
+++ app/assets/javascripts/lib/travis/log.js
@@ -1,22 +1,24 @@
 Travis.Log = {
   FOLDS: {
-    bundle:  /(^|<\/div>)(\$ bundle install.*\n+(?:(Fetching|Updating|Using|Installing).*?\n+)*)/gm,
-    migrate: /(^|<\/div>)(\$ (?:bundle exec )?rake [\s\S]*db:migrate[\s\S]*\n+(?:^== +\w+: migrated \(.*\) =+\n+))/gm,
-    schema:  /(^|<\/div>)(\$ (?:bundle exec )?rake db:schema:load[\s\S]*(?:^-- assume_migrated_upto_version[\s\S]*\n))/gm,
-    exec:    /(^|<\/div>)([\/\w]*.rvm\/rubies\/\S*?\/(ruby|rbx|jruby) [\s\S]*)/gm
+    schema:  /(<p.*?\/a>\$ (?:bundle exec )?rake( db:create)? db:schema:load[\s\S]*?<p.*?\/a>-- assume_migrated_upto_version[\s\S]*?<\/p>\n<p.*?\/a>.*<\/p>)/gm,
+    migrate: /(<p.*?\/a>\$ (?:bundle exec )?rake( db:create)? db:migrate[\s\S]*== +\w+: migrated \(.*\) =+)/gm,
+    bundle:  /(<p.*?\/a>\$ bundle install.*<\/p>\n(<p.*?\/a>(Updating|Using|Installing|Fetching|remote:|Receiving|Resolving).*?<\/p>\n|<p.*?\/a><\/p>\n)*)/gm,
+    exec:    /(<p.*?\/a>[\/\w]*.rvm\/rubies\/[\S]*?\/(ruby|rbx|jruby) .*?<\/p>)/g
   },
 
   filter: function(log) {
-    log = this.stripPaths(log);
+    // log = this.stripPaths(log);
     log = this.escapeHtml(log);
-    log = this.fold(log);
     log = this.deansi(log);
+    log = log.replace(/\r/g, '');
     log = this.numberLines(log);
+    log = this.fold(log);
+    log = log.replace(/\n/g, '');
     return log;
   },
 
   stripPaths: function(log) {
-    return log.replace(/\/tmp\/travis\/builds(\/[^\/\n]+){2}\//g, '');
+    return log.replace(/\/home\/vagrant\/builds(\/[^\/\n]+){2}\//g, '');
   },
 
   escapeHtml: function(log) {
@@ -29,9 +31,9 @@ Travis.Log = {
 
   numberLines: function(log) {
     var result = '';
-    $.each(log.split('\n'), function (ix, line) {
+    $.each(log.trim().split('\n'), function (ix, line) {
       var path = Travis.Log.location().replace(/\/L\d+/, '') + '/L' + (ix + 1);
-      result += '<p><a href="%@" name="%@">%@</a>%@</p>\n'.fmt(path, path, (ix + 1), line);
+      result += '<p><a href="%@" name="%@">%@</a>%@</p>\n'.fmt(path, path, (ix + 1), line.trim());
     })
     return result.trim();
   },
@@ -44,7 +46,7 @@ Travis.Log = {
     log = this.unfold(log);
     $.each(Travis.Log.FOLDS, function(name, pattern) {
       log = log.replace(pattern, function() {
-        return arguments[1] + '<div class="fold ' + name + '">' + arguments[2].trim() + '</div>';
+        return '<div class="fold ' + name + '">' + arguments[1].trim() + '</div>';
       });
     });
     return log;
--- app/assets/javascripts/spec/lib/travis/log/fold_pattern_spec.js
+++ app/assets/javascripts/spec/lib/travis/log/fold_pattern_spec.js
@@ -5,20 +5,21 @@ describe("Travis.Log", function() {
         var log = '$ bundle install\n' +
                   'Fetching source index for http://rubygems.org/\n' +
                   'Using rake (0.9.2)\n' +
-                  'Using diaspora-client (0.1.0) from git://github.com/diaspora/diaspora-client.git (at master)\n\n' +
-                  'Installing rcov (0.9.9) with native extensions WARNING: rcov-0.9.9 has an invalid nil value for @cert_chain\n'
+                  'Installing rcov (0.9.9) with native extensions WARNING: rcov-0.9.9 has an invalid nil value for @cert_chain\n\n' +
+                  'Using diaspora-client (0.1.0) from git://github.com/diaspora/diaspora-client.git (at master)\n' +
+                  'foo\nbar'
 
-        expect(log).toMatch(Travis.Log.FOLDS['bundle']);
+        expect(Travis.Log.numberLines(log)).toMatch(Travis.Log.FOLDS['bundle']);
       });
 
       it('matches with multiple line breaks', function() {
-        var log = '$ bundle install\n\n' +
+        var log = '$ bundle install\n' +
                   'Fetching source index for http://rubygems.org/\n\n' +
                   'Using rake (0.9.2)\n\n' +
-                  'Using diaspora-client (0.1.0) from git://github.com/diaspora/diaspora-client.git (at master)\n\n' +
-                  'Installing rcov (0.9.9) with native extensions WARNING: rcov-0.9.9 has an invalid nil value for @cert_chain\n\n'
+                  'Installing rcov (0.9.9) with native extensions WARNING: rcov-0.9.9 has an invalid nil value for @cert_chain\n\n' +
+                  'Using diaspora-client (0.1.0) from git://github.com/diaspora/diaspora-client.git (at master)\n\n'
 
-        expect(log).toMatch(Travis.Log.FOLDS['bundle']);
+        expect(Travis.Log.numberLines(log)).toMatch(Travis.Log.FOLDS['bundle']);
       });
     });
 
@@ -30,7 +31,7 @@ describe("Travis.Log", function() {
                   '   -> 0.0016s\n' +
                   '==  CreateSettings: migrated (0.0017s) ========================================\n';
 
-        expect(log).toMatch(Travis.Log.FOLDS['migrate']);
+        expect(Travis.Log.numberLines(log)).toMatch(Travis.Log.FOLDS['migrate']);
       });
 
       it('matches with single line breaks', function() {
@@ -46,7 +47,7 @@ describe("Travis.Log", function() {
                   '   -> 0.0016s\n' +
                   '==  CreateSettings: migrated (0.0017s) ========================================\n';
 
-        expect(log).toMatch(Travis.Log.FOLDS['migrate']);
+        expect(Travis.Log.numberLines(log)).toMatch(Travis.Log.FOLDS['migrate']);
       });
 
       it('matches with multiple line breaks', function() {
@@ -62,7 +63,7 @@ describe("Travis.Log", function() {
                   '   -> 0.0016s\n\n' +
                   '==  CreateSettings: migrated (0.0017s) ========================================\n\n';
 
-        expect(log).toMatch(Travis.Log.FOLDS['migrate']);
+        expect(Travis.Log.numberLines(log)).toMatch(Travis.Log.FOLDS['migrate']);
       });
     });
 
@@ -73,9 +74,10 @@ describe("Travis.Log", function() {
                   '   -> 0.2026s\n' +
                   '-- add_index("aspect_memberships", ["aspect_id"], {:name=>"index_aspect_memberships_on_aspect_id"})\n' +
                   '   -> 0.0444s\n' +
-                  '-- assume_migrated_upto_version(20110830170929, "db/migrate")\n'
+                  '-- assume_migrated_upto_version(20110830170929, "db/migrate")\n' +
+                  '   -> 0.0444s\n'
 
-        expect(log).toMatch(Travis.Log.FOLDS['schema']);
+        expect(Travis.Log.numberLines(log)).toMatch(Travis.Log.FOLDS['schema']);
       });
 
       it('matches with single line breaks', function() {
@@ -84,9 +86,10 @@ describe("Travis.Log", function() {
                   '   -> 0.2026s\n' +
                   '-- add_index("aspect_memberships", ["aspect_id"], {:name=>"index_aspect_memberships_on_aspect_id"})\n' +
                   '   -> 0.0444s\n' +
-                  '-- assume_migrated_upto_version(20110830170929, "db/migrate")\n'
+                  '-- assume_migrated_upto_version(20110830170929, "db/migrate")\n' +
+                  '   -> 0.0444s\n'
 
-        expect(log).toMatch(Travis.Log.FOLDS['schema']);
+        expect(Travis.Log.numberLines(log)).toMatch(Travis.Log.FOLDS['schema']);
       });
 
       it('matches with multiple line breaks', function() {
@@ -95,25 +98,24 @@ describe("Travis.Log", function() {
                   '   -> 0.2026s\n\n' +
                   '-- add_index("aspect_memberships", ["aspect_id"], {:name=>"index_aspect_memberships_on_aspect_id"})\n\n' +
                   '   -> 0.0444s\n' +
-                  '-- assume_migrated_upto_version(20110830170929, "db/migrate")\n'
+                  '-- assume_migrated_upto_version(20110830170929, "db/migrate")\n' +
+                  '   -> 0.0444s\n'
 
-        expect(log).toMatch(Travis.Log.FOLDS['schema']);
+        expect(Travis.Log.numberLines(log)).toMatch(Travis.Log.FOLDS['schema']);
       });
     });
 
     describe('for executing ruby lines', function() {
       it('matches lines like output by rake', function() {
-        var log = '/home/vagrant/.rvm/rubies/ree-1.8.7-2011.03/bin/ruby -S' +
-                  'bundle exec rspec ./spec/controllers/aspect_memberships_controller_spec.rb' +
-                  './spec/controllers/activity_streams/photos_controller_spec.rb';
+        var log = '/home/vagrant/.rvm/rubies/ree-1.8.7-2011.03/bin/ruby -S bundle exec rspec ./spec/controllers/aspect_memberships_controller_spec.rb ./spec/controllers/activity_streams/photos_controller_spec.rb';
 
-        expect(log).toMatch(Travis.Log.FOLDS['exec']);
+        expect(Travis.Log.numberLines(log)).toMatch(Travis.Log.FOLDS['exec']);
       });
 
       it('does not match other lines starting with a path to the rvm ruby dir', function() {
         var log = "/home/vagrant/.rvm/rubies/ruby-1.8.7-p334/lib/ruby/site_ruby/1.8/rubygems/spec_fetcher.rb:133:in `load': marshal data too short (ArgumentError)\r\n";
 
-        expect(log).not.toMatch(Travis.Log.FOLDS['exec']);
+        expect(Travis.Log.numberLines(log)).not.toMatch(Travis.Log.FOLDS['exec']);
       });
     });
   });
