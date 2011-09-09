f15de0635425f3b67d91034a45b04ab05579f8b4
Sven Fuchs <svenfuchs@artweb-design.de>
2011-09-09 04:04:44 +0200

    make sure build:finished updates the build

--- app/assets/javascripts/spec/events/queue_spec.js
+++ app/assets/javascripts/spec/events/queue_spec.js
@@ -1,7 +1,7 @@
 var EVENTS = {
-  'build:queued':    { build: { id: 11, number: '11.1' }, repository: { slug: 'travis-ci/travis-ci' } },
-  'build:removed':   { build: { id: 11 } },
-  'build:finished':  { build: { id: 11 } },
+  'build:queued':    { build: { id: 1, number: '11.1' }, repository: { slug: 'travis-ci/travis-ci' } },
+  'build:removed':   { build: { id: 1 } },
+  'build:finished':  { build: { id: 1, finished_at: '2011-11-11T01:00:00Z', status: 1 } },
   'build:started:1': { build: { id: 11, repository_id: 1,  number: '11' }, repository: { id: 1,  slug: 'travis-ci/travis-ci' } },
   'build:started:2': { build: { id: 11, repository_id: 11, number: '11' }, repository: { id: 11, slug: 'svenfuchs/minimal'   } },
   'build:log:1':     { build: { id: 2, _log: 'Foo.' } },
@@ -9,36 +9,31 @@ var EVENTS = {
 
 describe('Events:', function() {
   var events = Travis.Controllers.Events.create();
+  var queue;
   var receiveEvent = function(event, ix) {
     SC.run(function() { events.receive(event, EVENTS[$.compact([event, ix]).join(':')]); });
   };
 
-  describe('the queue', function() {
-    var view;
+  beforeEach(function() {
+    queue = createView('#jobs', { jobs: Travis.Job.all(), template: 'app/templates/jobs/list' });
+  });
 
-    beforeEach(function() {
-      view = createView('#jobs', { jobs: Travis.Job.all(), template: 'app/templates/jobs/list' });
-    });
-
-    it('an incoming build:queued event adds the job to the queue', function() {
+  describe('build:queued', function() {
+    it('adds the job to the queue', function() {
       receiveEvent('build:queued');
-      expect(view.$('ul').html()).toHaveText('travis-ci/travis-ci #11.1');
-    });
-
-    it('an incoming build:removed event removes the job from the queue', function() {
-      receiveEvent('build:queued');
-      receiveEvent('build:removed');
-      expect(view.$('ul').html()).toHaveText('No jobs');
-    });
-
-    it('an incoming build:finished event removes the job from the queue', function() {
-      receiveEvent('build:queued');
-      receiveEvent('build:finished');
-      expect(view.$('ul').html()).toHaveText('No jobs');
+      expect(queue.$('ul').html()).toHaveText('travis-ci/travis-ci #11.1');
     });
   });
 
-  describe('starting a build', function() {
+  describe('build:removed', function() {
+    it('build:removed event removes the job from the queue', function() {
+      receiveEvent('build:queued');
+      receiveEvent('build:removed');
+      expect(queue.$('ul').html()).toHaveText('No jobs');
+    });
+  });
+
+  describe('build:started', function() {
     it('adds the build to an existing repository', function() {
       var repository = Test.Factory.Repository.travis();
       var count  = function() { Travis.Repository.all().get('length') };
@@ -65,7 +60,7 @@ describe('Events:', function() {
     });
   });
 
-  describe('receiving log output', function() {
+  describe('build:log', function() {
     it('appends to an existing build', function() {
       var build = Test.Factory.Build.passing();
       var test = build.get('matrix').objectAt(0);
@@ -74,6 +69,24 @@ describe('Events:', function() {
       receiveEvent('build:log', 1);
 
       expect(test.get('log')).toEqual('Done. Build script exited with: 0\nFoo.');
+    });
+  });
+
+  describe('build:finished', function() {
+    it('removes the job from the queue', function() {
+      Test.Factory.Build.passing();
+      receiveEvent('build:queued');
+      receiveEvent('build:finished');
+      expect(queue.$('ul').html()).toHaveText('No jobs');
+    });
+
+    it('updates the build', function() {
+      var build = Test.Factory.Build.passing();
+      receiveEvent('build:queued');
+      receiveEvent('build:finished');
+
+      expect(build.get('result')).toEqual(1);
+      expect(build.get('finishedAt')).toEqual('2011-11-11T01:00:00Z');
     });
   });
 });
