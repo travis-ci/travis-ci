219f548af958c6a3ba377c1d594f851d0636f1fe
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2011-09-10 15:20:37 +0200

    Adding fake seeds.

---
+++ lib/forgery/forgeries/commit.rb
@@ -0,0 +1,27 @@
+class Forgery::Commit < Forgery
+  def self.commit
+    dictionaries[:commits].random
+  end
+  def self.branch
+    dictionaries[:commit_branches].random
+  end
+  def self.message
+    dictionaries[:commit_messages].random
+  end
+  def self.commiter_name
+    dictionaries[:commiter_names].random
+  end
+  def self.commiter_email
+    dictionaries[:commiter_emails].random
+  end
+  def self.compare_url
+    dictionaries[:commit_compare_urls].random
+  end
+end
+
+
+class Forgery::Build < Forgery
+  def self.config
+    dictionaries[:build_configs].random
+  end
+end
