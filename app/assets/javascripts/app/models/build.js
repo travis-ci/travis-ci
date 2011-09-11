284baf2a8e0d3e5bb76d0f7a082ae9e325cfc067
Sven Fuchs <svenfuchs@artweb-design.de>
2011-09-11 23:59:55 +0200

    try making the properties cacheable
    i somehow thought they were cacheable by default in sproutcore 2.0!

--- app/assets/javascripts/app/models/build.js
+++ app/assets/javascripts/app/models/build.js
@@ -20,92 +20,99 @@ Travis.Build = Travis.Record.extend(Travis.Helpers.Urls, Travis.Helpers.Common, 
 
   matrix: SC.Record.toMany('Travis.Build', { nested: true }), // TODO should be Travis.Test!
 
+  parent: function() {
+    //console.log('updating parent on build ' + this.get('id'));
+    return this.get('parentId') ? Travis.Build.find(this.get('parentId')) : null;
+  }.property('parentId', 'status').cacheable(),
+
+  repository: function() {
+    //console.log('updating repository on build ' + this.get('id'));
+    return Travis.Repository.find(this.get('repositoryId'));
+  }.property('repositoryId', 'status').cacheable(),
+
+  build: function() {
+    return this.get('matrix').objectAt(0);
+  }.property('build', 'status').cacheable(),
+
   update: function(attrs) {
     if('status' in attrs) attrs.result = attrs.status
     if('matrix' in attrs) attrs.matrix = this._joinMatrixAttributes(attrs.matrix);
     this._super(attrs);
   },
 
-  parent: function() {
-    return this.get('parentId') ? Travis.Build.find(this.get('parentId')) : null;
-  }.property('parentId', 'status'),
-
-  repository: function() {
-    return Travis.Repository.find(this.get('repositoryId'));
-  }.property('repositoryId'),
-
-  isMatrix: function() {
-    return this.getPath('matrix.length') > 1;
-  }.property('matrix.status'),
-
   appendLog: function(log) {
     this.set('log', this.get('log') + log);
   },
 
+  updateTimes: function() {
+    this.notifyPropertyChange('startedAt');
+    this.notifyPropertyChange('finishedAt');
+  },
+
+  isMatrix: function() {
+    //console.log('updating isMatrix on build ' + this.get('id'));
+    return this.getPath('matrix.length') > 1;
+  }.property('matrix.status').cacheable(),
+
   color: function() {
+    //console.log('updating color on build ' + this.get('id'));
     return this.colorForStatus(this.get('result'));
-  }.property('status', 'result'),
+  }.property('status', 'result').cacheable(),
 
   duration: function() {
+    //console.log('updating duration on build ' + this.get('id'));
     return this.durationFrom(this.get('startedAt'), this.get('finishedAt'));
-  }.property('startedAt', 'finishedAt'),
+  }.property('startedAt', 'finishedAt').cacheable(),
 
   configKeys: function() {
     return $.map($.keys($.only(this.get('config'), 'rvm', 'gemfile', 'env', 'otp_release')), function(key) { return $.camelize(key) });
-  }.property('config'),
+  }.property('config').cacheable(),
 
   configValues: function() {
     return $.values($.only(this.get('config'), 'rvm', 'gemfile', 'env', 'otp_release'));
-  }.property('config'),
+  }.property('config').cacheable(),
 
   // see https://github.com/sproutcore/sproutcore20/issues/160
   configKeyObjects: function() {
+    //console.log('updating configKeyObjects on build ' + this.get('id'));
     return $.map(this.get('configKeys'), function(key) { return SC.Object.create({ key: key }) });
-  }.property('config'),
+  }.property('config').cacheable(),
 
   configValueObjects: function() {
+    //console.log('updating configValueObjects on build ' + this.get('id'));
     return $.map(this.get('configValues'), function(value) { return SC.Object.create({ value: value }) });
-  }.property('config'),
+  }.property('config').cacheable(),
 
   // TODO the following display logic all seems to belong to a controller or helper module
 
   formattedCommit: function() {
     return (this.get('commit') || '').substr(0, 7) + (this.get('branch') ? ' (%@)'.fmt(this.get('branch')) : '');
-  }.property('commit', 'branch'),
+  }.property('commit', 'branch').cacheable(),
 
   formattedDuration: function() {
     return this.readableTime(this.get('duration'));
-  }.property('status', 'duration'),
+  }.property('status', 'duration').cacheable(),
 
   formattedFinishedAt: function() {
     return this.timeAgoInWords(this.get('finishedAt')) || '-';
-  }.property('status', 'finishedAt'),
+  }.property('status', 'finishedAt').cacheable(),
 
   formattedConfig: function() {
     if(this.get('isMatrix')) return;
     var config = $.only(this.get('config'), 'rvm', 'gemfile', 'env');
     var values = $.map(config, function(value, key) { return '%@: %@'.fmt($.camelize(key), value.join ? value.join(', ') : value); });
     return values.length == 0 ? '-' : values.join(', ');
-  }.property('config'),
+  }.property('config').cacheable(),
 
   formattedLog: function() {
     var log = this.get('parentId') ? this.get('log') : this.getPath('matrix.firstObject.log');
     return log ? Travis.Log.filter(log) : '';
-  }.property('matrix', 'log'),
+  }.property('matrix', 'log').cacheable(),
 
   formattedCompareUrl: function() {
     var parts = (this.get('compare_url') || '').split('/');
     return parts[parts.length - 1];
-  }.property('compareUrl'),
-
-  build: function() {
-    return this.get('matrix').objectAt(0);
-  }.property('build'),
-
-  updateTimes: function() {
-    this.notifyPropertyChange('startedAt');
-    this.notifyPropertyChange('finishedAt');
-  },
+  }.property('compareUrl').cacheable(),
 
   // need to join given attributes with existing attributes because SC.Record.toMany
   // does not seem to allow partial updates, i.e. would remove existing attributes?
