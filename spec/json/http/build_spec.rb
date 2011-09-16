adf236a26da524fc3ba0890c8080682704989f65
Chris Harper <charper@branched.co.uk>
2011-09-16 21:04:18 +0100

    Fix formatting issues

--- spec/json/http/build_spec.rb
+++ spec/json/http/build_spec.rb
@@ -12,20 +12,22 @@ describe 'HTTP API for Build' do
       'id' => build.id,
       'repository_id' => repository.id,
       'number' => '2',
-      'config' => { 'rvm' => ['1.8.7', '1.9.2'], 'gemfile' => ['test/Gemfile.rails-2.3.x', 'test/Gemfile.rails-3.0.x'] },
       'state' => 'finished',
-      'result' => 0,
       'started_at' => '2010-11-12T12:30:00Z',
       'finished_at' => '2010-11-12T12:30:20Z',
+      'config' => { 'rvm' => ['1.8.7', '1.9.2'], 'gemfile' => ['test/Gemfile.rails-2.3.x', 'test/Gemfile.rails-3.0.x'] },
+      'status' => 0,
+      'result' => 0,
       'commit' => '91d1b7b2a310131fe3f8',
       'branch' => 'master',
-      'compare_url' => 'https://github.com/svenfuchs/minimal/compare/master...develop',
       'message' => 'Bump to 0.0.22',
+      'compare_url' => 'https://github.com/svenfuchs/minimal/compare/master...develop',
       'committed_at' => '2010-11-12T12:25:00Z',
       'committer_email' => 'svenfuchs@artweb-design.de',
       'committer_name' => 'Sven Fuchs',
       'author_name' => 'Sven Fuchs',
       'author_email' => 'svenfuchs@artweb-design.de',
+      "compare_url"=>"https://github.com/svenfuchs/minimal/compare/master...develop"
     }
     json['matrix'].first.should == build.matrix.map { |task| json_for_http(task) }.first
   end
