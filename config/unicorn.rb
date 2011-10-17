3074208782f92941a298ccc41b5e6e06ba9f2f19
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-10-17 22:44:56 +0200

    we need to reestablish the connection after forking

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,7 +5,15 @@ timeout 15         # restarts workers that hang for 15 seconds
 
 preload_app true
 
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
+end
+
 after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.establish_connection
+
   require 'travis'
   Travis::Amqp.setup_connection
 end
