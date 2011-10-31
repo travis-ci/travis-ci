7ea90b114d7ce7ab1482c280b5a3eb48636b5e6d
Sven Fuchs <svenfuchs@artweb-design.de>
2011-10-31 23:53:47 +0100

    move mailer helper spec

---
+++ spec_fast/travis/mailer/helper/build_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Travis::Mailer::Helper::Build do
+  # let(:build) { Factory(:successful_build) }
+
+  # it '#title returns title for the build' do
+  #   title(build).should == 'Build Update for svenfuchs/successful_build'
+  # end
+end
+
