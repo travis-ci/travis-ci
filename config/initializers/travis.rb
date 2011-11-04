265ae9cdbd13631e8f561dfc9958bbb51c245a31
Sven Fuchs <svenfuchs@artweb-design.de>
2011-11-04 03:04:51 +0100

    move Pusher setup to Nofifications::Pusher

--- config/initializers/travis.rb
+++ config/initializers/travis.rb
@@ -3,10 +3,3 @@ require 'travis'
 if Travis.config.redis?
   Resque.redis = Travis.config.redis.url
 end
-
-if Travis.config.pusher?
-  Pusher.app_id = Travis.config.pusher.app_id
-  Pusher.key    = Travis.config.pusher.key
-  Pusher.secret = Travis.config.pusher.secret
-  Travis.pusher = Pusher
-end
