d694777b7c2ec809bd704601dabf9906f8ad4fa6
Sven Fuchs <svenfuchs@artweb-design.de>
2011-11-04 13:24:20 +0100

    log with Thread.current.object_id

--- lib/travis/consumer.rb
+++ lib/travis/consumer.rb
@@ -26,6 +26,7 @@ module Travis
     end
 
     def receive(message, payload)
+      puts "[#{Thread.current.object_id}] Handling event #{message.type.inspect} with payload : #{payload.inspect}"
       event   = message.type
       handler = handler_for(event)
       handler.handle(event, decode(payload))
