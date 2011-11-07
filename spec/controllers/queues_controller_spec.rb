2138113b4e94986c43697ff0e95b26ede304c421
Stefan Sprenger <info@stefan-sprenger.com>
2011-11-07 21:13:43 +0100

    Use Job#queue for selecting jobs of a given queue

--- spec/controllers/queues_controller_spec.rb
+++ spec/controllers/queues_controller_spec.rb
@@ -4,7 +4,7 @@ describe QueuesController, :type => :controller do
   let!(:jobs) { [Factory.create(:test, :number => '3'), Factory.create(:test, :number => '3.1') ] }
 
   it 'index lists all jobs on the queue' do
-    get :index, :format => :json
+    get :index, :format => :json, :queue => "ruby"
 
     json = ActiveSupport::JSON.decode(response.body)
     json.should include({ 'id' => jobs.first.id,  'number' => '3',   'commit' => '62aae5f70ceee39123ef', 'repository' => { 'id' => jobs.first.repository.id,  'slug' => jobs.first.repository.slug  } })
