79aa9c335619f39e9acb7c744705a6631cf18def
Stefan Sprenger <info@stefan-sprenger.com>
2011-11-07 15:17:10 +0100

    Use state :created instead of :started for selecting queued jobs

--- spec/controllers/queues_controller_spec.rb
+++ spec/controllers/queues_controller_spec.rb
@@ -1,23 +1,13 @@
 require 'spec_helper'
 
 describe QueuesController, :type => :controller do
-  before do
-    # TODO shouldn't this use the actual output of the json rendering?
-    repository = { 'id' => 8, 'slug' => 'svenfuchs/gem-release' }
-    build_3    = { 'id' => 1, 'number' => '3',   'commit' => 'b0a1b69', 'config' => {} }
-    build_31   = { 'id' => 2, 'number' => '3.1', 'commit' => 'b0a1b69', 'config' => {} }
-  end
-
-  subject do
-    get :index, :format => :json
-    ActiveSupport::JSON.decode(response.body)
-  end
+  let(:jobs) { [Factory.create(:test, :number => '3'), Factory.create(:test, :number => '3.1') ] }
 
   it 'index lists all jobs on the queue' do
-    pending
-    should == [
-      { 'id' => 1, 'number' => '3',   'commit' => 'b0a1b69', 'repository' => { 'id' => 8, 'slug' => 'svenfuchs/gem-release' } },
-      { 'id' => 2, 'number' => '3.1', 'commit' => 'b0a1b69', 'repository' => { 'id' => 8, 'slug' => 'svenfuchs/gem-release' } },
-    ]
+    get :index, :format => :json
+
+    json = ActiveSupport::JSON.decode(response.body)
+    json.should include({ 'id' => jobs.first.id,  'number' => '3',   'commit' => '62aae5f70ceee39123ef', 'repository' => { 'id' => jobs.first.repository.id,  'slug' => 'svenfuchs/repository-1' } })
+    json.should include({ 'id' => jobs.second.id, 'number' => '3.1', 'commit' => '62aae5f70ceee39123ef', 'repository' => { 'id' => jobs.second.repository.id, 'slug' => 'svenfuchs/repository-2' } })
   end
 end
