4e0eb63d850cfd509229991623adcef71bd5ff22
Sven Fuchs <svenfuchs@artweb-design.de>
2011-11-07 16:21:08 +0100

    move jobs from application_controller to queues_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,23 +13,6 @@ class ApplicationController < ActionController::Base
     end
     helper_method :repositories
 
-    def jobs(queue=nil)
-      queue   = Travis::Notifications::Worker.queues.select { |queue| queue.name == queue_name }.first
-      queue ||= Travis::Notifications::Worker.default_queue
-      queue.jobs.collect do |job|
-        {
-          :id         => job.id,
-          :number     => job.number,
-          :commit     => job.commit.commit,
-          :repository => {
-            :id   => job.repository.id,
-            :slug => job.repository.slug
-          }
-        }
-      end
-    end
-    helper_method :jobs
-
     def set_gitsha_header
       headers['X-GIT_SHA'] = ENV['GIT_SHA'] if ENV['GIT_SHA']
     end
--- app/controllers/queues_controller.rb
+++ app/controllers/queues_controller.rb
@@ -2,6 +2,34 @@ class QueuesController < ApplicationController
   respond_to :json
 
   def index
-    respond_with jobs(params[:queue])
+    respond_with jobs
   end
+
+  protected
+
+    def jobs
+      queue.jobs.map do |job|
+        {
+          :id         => job.id,
+          :number     => job.number,
+          :commit     => job.commit.commit,
+          :repository => {
+            :id   => job.repository.id,
+            :slug => job.repository.slug
+          }
+        }
+      end
+    end
+
+    def queue
+      queues.detect { |queue| queue.name == params[:queue] } || default_queue
+    end
+
+    def queues
+      Travis::Notifications::Worker.queues
+    end
+
+    def default_queue
+      Travis::Notifications::Worker.default_queue
+    end
 end
