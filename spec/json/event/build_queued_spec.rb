a9cc7c3ffc6150785f5fd77e1b4a0df8b58a5326
Sven Fuchs <svenfuchs@artweb-design.de>
2011-11-08 05:59:32 +0100

    expect the queue to be part of the pusher build:queued message

--- spec/json/event/build_queued_spec.rb
+++ spec/json/event/build_queued_spec.rb
@@ -7,10 +7,13 @@ describe 'JSON for websocket events' do
   let(:job) { build.matrix.first }
 
   it 'build:queued' do
+    job.update_attributes!(:queue => 'builds.common')
+
     json_for_pusher('build:queued', job).should == {
      'build' => {
        'id' => job.id,
-       'number' => '2.1'
+       'number' => '2.1',
+       'queue' => 'builds.common'
       },
       'repository' => {
         'id' => repository.id,
