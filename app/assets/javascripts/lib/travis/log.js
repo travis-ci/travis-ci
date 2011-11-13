e8b7490f16a7836ef375bdc9a2777bb8602f4079
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2011-11-13 15:14:04 +0100

    Best implementation of line numbers i could do :/ Unfortunately, I can't see straight/easy ways to determine when page was completely loaded, and so far got not much time to dig into internals of SC.

--- app/assets/javascripts/lib/travis/log.js
+++ app/assets/javascripts/lib/travis/log.js
@@ -32,8 +32,8 @@ Travis.Log = {
   numberLines: function(log) {
     var result = '';
     $.each(log.trim().split('\n'), function (ix, line) {
-      var path = Travis.Log.location().replace(/\/L\d+/, '') + '/L' + (ix + 1);
-      result += '<p><a href="%@" name="L%@">%@</a>%@</p>\n'.fmt(path, ix, (ix + 1), line.trim());
+      var path = Travis.Log.location().substr(1).replace(/\/L\d+/, '') + '/L' + (ix + 1);
+      result += '<p><a href="#%@" id="%@" name="L%@">%@</a>%@</p>\n'.fmt(path, path, (ix + 1), (ix + 1), line.trim());
     });
     return result.trim();
   },
