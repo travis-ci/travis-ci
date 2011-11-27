c5c5e1b50c3dabb10d6ae24b33eb6804303af597
Sven Fuchs <svenfuchs@artweb-design.de>
2011-11-27 01:40:20 +0100

    standardize on UTC

--- play/em-http.rb
+++ play/em-http.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'em-http'
 
 EventMachine.run {
-  http = EventMachine::HttpRequest.new('http://127.0.0.1:3000/builds/16').post :body => {:started_at => Time.now}
+  http = EventMachine::HttpRequest.new('http://127.0.0.1:3000/builds/16').post :body => { :started_at => Time.now.utc }
 
   http.callback {
     p http.response_header.status
