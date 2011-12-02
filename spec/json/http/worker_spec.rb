b563ec61f068cc4b2260b7a4d147878a9ef902c9
Sven Fuchs <svenfuchs@artweb-design.de>
2011-12-02 02:47:49 +0100

    commit missing changes to failing json specs

--- spec/json/http/worker_spec.rb
+++ spec/json/http/worker_spec.rb
@@ -14,7 +14,9 @@ describe 'Worker' do
       'name' => 'worker-1',
       'host' => 'ruby-1.workers.travis-ci.org',
       'state' => 'working',
-      'last_seen_at' => '2011-11-11T11:11:11Z'
+      'last_seen_at' => '2011-11-11T11:11:11Z',
+      'payload' => nil,
+      'last_error' => nil
     }]
   end
 end
