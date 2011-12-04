bee3332d0cdb64ecb632fccdddda7d8e29a83ab2
Pavel Forkert <fxposter@gmail.com>
2011-12-04 14:59:58 +0200

    Revert "Revert "Removed devise oauth in migration""
    This reverts commit 66eaabd008c382a0f4e5bf34a48f89fddd1416f6.

--- db/migrate/20110109130532_devise_create_users.rb
+++ db/migrate/20110109130532_devise_create_users.rb
@@ -4,12 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :login
       t.string :email
-      t.oauth2_authenticatable
       t.timestamps
     end
 
     add_index :users, :login, :unique => true
-    add_index :users, :oauth2_uid, :unique => true
   end
 
   def self.down
