d6ca3f65be03724d96d7545ffd4d7be92f07a2e4
Sven Fuchs <svenfuchs@artweb-design.de>
2011-12-06 08:07:22 +0100

    update specs for renamed Notifications Handler classes

--- db/migrate/archive_builds.rb
+++ db/migrate/archive_builds.rb
@@ -1,5 +1,5 @@
 require File.expand_path('config/environment')
 
 Build.where(:archived_at => nil).includes(:matrix => log).order('id DESC').limit(50).each do |build|
-  Travis::Notifications::Archive.new.send(:archive, build)
+  Travis::Notifications::Handler::Archive.new.send(:archive, build)
 end
