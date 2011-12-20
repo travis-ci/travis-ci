389e353f65893bb0fc5ad017f3e8cde65192cd73
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-12-20 02:25:59 +0100

    move the refraction middleware insertion point

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,5 +73,5 @@ TravisCi::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.middleware.insert_before(::Rack::Lock, 'Refraction')
+  config.middleware.insert_before(::Rack::Runtime, 'Refraction')
 end
