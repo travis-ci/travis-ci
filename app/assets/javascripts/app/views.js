2ecd482e1b081968f9222534cade0a8b37c93187
Josh Kalderimis <josh.kalderimis@gmail.com>
2012-01-05 01:41:58 +0100

    remove views.js and change the templates to use Ember.View and not Travis.View

--- app/assets/javascripts/app/views.js
+++
@@ -1,5 +0,0 @@
-// TODO it should be possible to remove this, but somehow that brakes
-// everything. It seems like nothing's using Travis.View anymore, but maybe
-// there is.
-Travis.View = Ember.View.extend();
-
