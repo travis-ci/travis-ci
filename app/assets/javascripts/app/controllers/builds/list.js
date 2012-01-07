5c9450806d2aa78220ea3edb44884421f42bafe5
Josh Kalderimis <josh.kalderimis@gmail.com>
2012-01-07 15:25:03 +0100

    and now we have working builds pagination!! :smiley: :bowtie: :grin:

--- app/assets/javascripts/app/controllers/builds/list.js
+++ app/assets/javascripts/app/controllers/builds/list.js
@@ -1,56 +1,42 @@
 Travis.Controllers.Builds.List = Ember.ArrayController.extend({
-  parent: null,
-  repositoryBinding: 'parent.repository',
-  defaultContentBinding: 'parent.repository.builds',
-  content: Ember.A(),
+  parent: null,
+  repositoryBinding: 'parent.repository',
+  contentBinding: 'parent.repository.builds',
 
-  init: function() {
-    this._super();
-    Ember.run.later(this.updateTimes.bind(this), Travis.UPDATE_TIMES_INTERVAL);
+  init: function() {
+    this._super();
+    Ember.run.later(this.updateTimes.bind(this), Travis.UPDATE_TIMES_INTERVAL);
 
-    this.view = Ember.View.create({
-      builds: this,
-      repositoryBinding: 'builds.repository',
-      templateName: 'app/templates/builds/list'
-    });
+    this.view = Ember.View.create({
+      builds: this,
+      repositoryBinding: 'builds.repository',
+      templateName: 'app/templates/builds/list'
+    });
+  },
 
-    this.propertyDidChange('defaultContent');
-  },
+  destroy: function() {
+    // console.log('destroying list in: ' + this.selector + ' .details')
+    if(this.view) {
+      this.view.$().remove();
+      this.view.destroy();
+    }
+  },
 
-  defaultContentDidChange: function() {
-    var content = this.get('defaultContent');
-    if (content && (content.get('status') & SC.Record.READY) && this.getPath('content.length') === 0) {
-      this.get('content').pushObjects(content.toArray());
-    }
-  }.observes('defaultContent.status'),
+  updateTimes: function() {
+    var builds  = this.get('builds');
+    if(builds) {
+      $.each(builds, function(ix, build) { build.updateTimes(); }.bind(this));
+    }
+    Ember.run.later(this.updateTimes.bind(this), Travis.UPDATE_TIMES_INTERVAL);
+  },
 
-  destroy: function() {
-    // console.log('destroying list in: ' + this.selector + ' .details')
-    if(this.view) {
-      this.view.$().remove();
-      this.view.destroy();
-    }
-  },
+  showMore: function() {
+    var id = this.getPath('repository.id'),
+      number = this.getPath('content.lastObject.number');
+    Travis.Build.olderThanNumber(id, number);
+  },
 
-  updateTimes: function() {
-    var builds  = this.get('builds');
-    if(builds) {
-      $.each(builds, function(ix, build) { build.updateTimes(); }.bind(this));
-    }
-    Ember.run.later(this.updateTimes.bind(this), Travis.UPDATE_TIMES_INTERVAL);
-  },
-
-  showMore: function() {
-    var content = this.get('content'),
-        lastBuild = content[content.length - 1],
-        moreContent = Travis.Build.olderThanNumber(this.getPath('repository.id'), lastBuild.get('number')),
-        moreContentDidLoad = function() {
-          if (moreContent.get('status') & SC.Record.READY) {
-            moreContent.removeObserver('status', this, moreContentDidLoad);
-            content.pushObjects(moreContent.toArray());
-          }
-        };
-    moreContent.addObserver('status', this, moreContentDidLoad);
-    moreContent.propertyDidChange('status');
-  }
+  showMoreIsVisibleBinding: Em.Binding.oneWay('content.lastObject.number').transform(function(value) {
+    return value > 1;
+  }),
 });
