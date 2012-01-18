c985f08c63bdb7a03b414a3e3519a083e35d71f9
Josh Kalderimis <josh.kalderimis@gmail.com>
2012-01-18 22:14:52 +0100

    the BuildsController#show method is called in both nested and non-nested circumstances, this fix allows it to be scoped properly when called in nested mode while also not interferring with url calls like /builds/:id

--- spec/controllers/builds_controller_spec.rb
+++ spec/controllers/builds_controller_spec.rb
@@ -15,17 +15,28 @@ describe BuildsController do
   end
 
   describe 'GET :show' do
-    it 'returns build details in json' do
-      build = builds.first
-      get :show, :repository_id => repository.id, :id => build.id, :format => :json
-      json_response.should == json_for_http(build)
+
+    context 'when called using /builds/:id' do
+      it 'returns build details in json' do
+        build = builds.first
+        get :show, :id => build.id, :format => :json
+        json_response.should == json_for_http(build)
+      end
     end
 
-    it 'returns 404 with wrong repoid' do
-      repoid = repository.id + 1
-      lambda {
-        get :show, :repository_id => repoid, :id => builds.first.id, :format => :json
-      }.should raise_error ActiveRecord::RecordNotFound
+    context 'when called in a nested repositories call eg. /svenfuchs/i18n/builds/:id' do
+      it 'returns build details in json' do
+        build = builds.first
+        get :show, :repository_id => repository.id, :id => build.id, :format => :json
+        json_response.should == json_for_http(build)
+      end
+
+      it 'returns 404 with wrong repoid' do
+        repoid = repository.id + 1
+        lambda {
+          get :show, :repository_id => repoid, :id => builds.first.id, :format => :json
+        }.should raise_error ActiveRecord::RecordNotFound
+      end
     end
   end
 end
