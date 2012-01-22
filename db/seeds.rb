32900c473a6ed5efab36403ea6455d530a587d60
Sven Fuchs <svenfuchs@artweb-design.de>
2012-01-22 15:35:19 +0100

    stop rake db:seed from pushing notifications to Worker and Pusher [ci skip]

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@ require 'forgery'
 Dir["#{Rails.root}/lib/forgery/forgeries/*.rb"].each {|f| require f}
 Dir["#{Rails.root}/spec/support/factories/*.rb"].each {|f| require f}
 
+Travis.config.notifications = []
+
 if Rails.env.development? || Rails.env.jasmine?
   ActiveRecord::Base.connection.tables.each do |table|
     ActiveRecord::Base.connection.execute("TRUNCATE #{table}") if table != "schema_migrations"
@@ -21,9 +23,11 @@ if Rails.env.development? || Rails.env.jasmine?
   elsif Rails.env.development?
     10.times do
       repository = FactoryGirl.create(:seed_repository)
+
       3.times do
         build = FactoryGirl.create(:seed_build, :repository => repository)
       end
+
       repository.last_build_id = repository.last_build.id
       repository.save
     end
