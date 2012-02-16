128f480fd8c22d484bcec73b70358da462a7b6d2
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-02-16 16:34:12 +0100

    track X-Github-Event header for incoming events

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,7 +1,7 @@
 class RequestsController < ApplicationController
   # also responds to POST /builds legacy route
   def create
-    Request.create_from(params[:payload], api_token)
+    Request.create_from(request.env['HTTP_X_GITHUB_EVENT'], params[:payload], api_token)
     render :nothing => true
   end
 
