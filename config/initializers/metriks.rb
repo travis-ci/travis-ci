6875c0e66f9cfddb6d22955c5b9cb850c9fe7239
Mathias Meyer <meyer@paperplanes.de>
2012-02-23 20:32:25 +0100

    Move report initializer into Unicorn config.

--- config/initializers/metriks.rb
+++ config/initializers/metriks.rb
@@ -1,6 +1,3 @@
 require 'metriks/reporter/logger'
 require 'remote_syslog_logger'
-logger = RemoteSyslogLogger.new(ENV['SYSLOG_HOST'], ENV['SYSLOG_PORT'])
-$reporter = Metriks::Reporter::Logger.new :logger => logger, :interval => 5
-$reporter.start
 Metriks.counter("starts").increment
