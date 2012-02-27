02da2648e5e841668e39c42330cc142ab838f171
Randy Morgan <randym@reallyenglish.com>
2012-02-28 01:10:26 +0900

    events_spec cleanup

--- app/assets/javascripts/spec/events_spec.js
+++ app/assets/javascripts/spec/events_spec.js
@@ -1,13 +1,4 @@
 var EVENTS = {
-/* These events no longer exist in master
-  'build:queued':    { build: { id: 1, number: '11.1' }, repository: { slug: 'travis-ci/travis-ci' } },
-  'build:removed':   { build: { id: 1 } },
-*/
-  'build:finished':  { build: { id: 1, finished_at: '2011-11-11T01:00:00Z', result: 1 }, repository: { id: 1,  slug: 'travis-ci/travis-ci' } },
-
-  'build:started:1': { build: { id: 11, repository_id: 1,  number: '11' }, repository: { id: 1,  slug: 'travis-ci/travis-ci' } },
-
-  'build:started:2': { build: { id: 12, repository_id: 12, number: '12' }, repository: { id: 12, slug: 'svenfuchs/minimal'   } },
 
   'job:log':     { id: 9, _log: 'Foo.' },
 
@@ -15,11 +6,16 @@ var EVENTS = {
 
   'job:started': { id: 9 },
 
-  'job:finished': { id: 9 }
+  'job:finished': { id: 9 },
+
+  'build:started': { build: { id: 2, repository_id: 2, number: 11 }, repository: { id: 2, slug: 'travis-ci/travis-ci' } },
+
+  'build:finished':  { build: { id: 2, repository_id: 2, number: 11, result: 1 }, repository: { id: 2,  slug: 'travis-ci/travis-ci-changed' } }
 
 };
 
 describe('Events:', function() {
+
   var events = Travis.Controllers.Events.create();
   var queue;
 
@@ -29,16 +25,12 @@ describe('Events:', function() {
     Ember.run(function() { events.receive(event, data); });
   };
 
-  /*
-
-
-
-  */
   describe('job Events', function() {
     it('appends log data to an existing jobs log', function() {
       var test = Test.Factory.Job.single();
       receiveEvent('job:log');
       expect(test.get('log')).toEqual('Done. Build script exited with: 0\nFoo.');
+
     });
 
     it('adds a job when a new job is created', function() {
@@ -61,38 +53,32 @@ describe('Events:', function() {
     });
   });
 
-  describe('build Events', function() {
-    it('adds the build to an existing repository', function() {
-      var repository = Test.Factory.Repository.travis();
-      var count  = function() { Travis.Repository.all().get('length') };
-      var before = count();
-      
-      receiveEvent('build:started', 1);
-      
-      var build = Travis.Build.find(11);
-      expect(build.get('number')).toEqual(11);
-      expect(build.getPath('repository.slug')).toEqual('travis-ci/travis-ci');
-      expect(count()).toEqual(before);
-    });
+  describe('updateFrom', function() {
+    it('creates and updates repositories and builds', function() {
 
-    it('adds the build to a new repository', function() {
-      var count  = function() { Travis.Repository.all().get('length') };
-      var before = count();
-      
-      receiveEvent('build:started', 2);
-      
-      var build = Travis.Build.find(12);
-      expect(build.get('number')).toEqual(12);
-      expect(build.getPath('repository.slug')).toEqual('svenfuchs/minimal');
-      expect(count()).toEqual(before);
-    });
-    
-    it('updates the build', function() {
-      var build = Test.Factory.Build.passing();
+      expect(Travis.Repository.all().get('length')).toEqual(0);
+      expect(Travis.Build.all().get('length')).toEqual(0);
+
+      receiveEvent('build:started');
+      var def = EVENTS['build:started']
+      //added
+      expect(Travis.Repository.all().get('length')).toEqual(1);
+      expect(Travis.Build.all().get('length')).toEqual(1);
+
+      //accurate
+      var build = Travis.store.find(Travis.Build,def.build.id)
+      var repo = Travis.store.find(Travis.Repository, def.repository.id)
+      expect(build.get('number')).toEqual(def.build.number);
+      expect(repo.get('slug')).toEqual(def.repository.slug)
+      expect(build.get('result')).toEqual(null);
+
+      //updated
       receiveEvent('build:finished');
-      
-      expect(build.get('result')).toEqual(1);
-      expect(build.get('finished_at')).toEqual('2011-11-11T01:00:00Z');
-    });    
+      def = EVENTS['build:finished']
+      build = Travis.store.find(Travis.Build,def.build.id)
+      expect(build.get('result')).toEqual(def.build.result);
+
+    });
   });
+
 });
