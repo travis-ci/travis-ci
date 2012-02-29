ca99face1b231222234031c2c84f53c550b29f5e
Sven Fuchs <svenfuchs@artweb-design.de>
2012-02-29 17:47:10 +0100

    work around 3.2 issues with require_tree including our hjs templates on assets:precompile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,9 @@
 //= require vendor
 //= require app
 //= require_tree ./lib
-//= require_tree ./app
+//= require_tree ./app/controllers
+//= require_tree ./app/helpers
+//= require_tree ./app/models
 //= require sponsors
 //= require_tree ./sponsors
 
