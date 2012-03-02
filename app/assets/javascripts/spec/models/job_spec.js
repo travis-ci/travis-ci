4133a1dc86f54c7ebff8a4202ae82ae8d0a58171
Randy Morgan <randym@reallyenglish.com>
2012-03-03 00:08:16 +0900

    complete specs for job model

--- app/assets/javascripts/spec/models/job_spec.js
+++ app/assets/javascripts/spec/models/job_spec.js
@@ -1,127 +1,165 @@
 describe('Job', function() {
-
+   var job, build;
   describe('instance', function() {
-    var repository, build;
-
     beforeEach(function() {
-      repository = Test.Factory.Repository.travis();
-      build = Test.Factory.Job.single();
+      //this will create the travis repostiory and a passing build.
+      job = Test.Factory.Job.single()
+      build = Test.Factory.Build.passing()
     });
 
     describe('associations', function() {
-      it('has many tests as a matrix', function() {
-        expect(build.get('number')).toEqual('1.9');
+      it('has a build', function() {
+        expect(job.get('build').get('number')).toEqual(1);
       });
 
       it('belongs to a repository', function() {
-        expect(repository.get('slug')).toEqual(repository.get('slug'));
+        expect(job.get('repository').get('slug')).toEqual(build.get('repository').get('slug'));
       });
     });
 
     describe('properties', function() {
       describe('color', function() {
         it('returns "green" if the last build has passed', function() {
-          build.set('result', 0);
-          expect(build.get('color')).toEqual('green');
+          job.set('result', 0);
+          expect(job.get('color')).toEqual('green');
         });
 
         it('returns "red" if the last build has failed', function() {
-          build.set('result', 1);
-          expect(build.get('color')).toEqual('red');
+          job.set('result', 1);
+          expect(job.get('color')).toEqual('red');
         });
 
         it('returns undefined if the last build result is unknown', function() {
-          build.set('result', null);
-          expect(build.get('color')).toEqual(undefined);
+          job.set('result', null);
+          expect(job.get('color')).toEqual(undefined);
         });
       });
 
+      describe('duration', function() {
+	it('returns a string for duration of the job', function() {
+	  expect(job.get('duration')).toEqual(10)
+	});
+
+	it('returns a duration of 0 if the job has not started or ended', function() {
+	  job.set('started_at', null)
+	  expect(job.get('duration')).toEqual(0)
+	});
+      });
+
+      describe('formattedDuration', function() {
+	it('returns the properly formatted string when the job is finished', function() {
+	  expect(job.get('formattedDuration')).toEqual('10 sec')
+	});
+
+	it('returns - when he job has not started yet', function() {
+	  job.set('started_at', null)
+	  expect(job.get('formattedDuration')).toEqual('-')
+	});
+      });
+
+      describe('formattedFinishedAt', function() {
+	it('returns a string for duration of the job', function() {
+	  expect(job.get('formattedFinishedAt').indexOf('ago')).toBeGreaterThan(0);
+	});
+
+	it('returns a duration of 0 if the job has not started or ended', function() {
+	  job.set('finished_at', null)
+	  expect(job.get('formattedFinishedAt')).toEqual('-')
+	});
+      });
+
+
+      describe('formattedCommit', function() {
+	it('returns a commit message formatted with the branch name', function() {
+	  expect(job.get('formattedCommit')).toEqual('4d7621e (master)')
+	});
+
+	it('returns an empty string when there is no commit meesage', function() {
+	  job.set('commit', null)
+	  job.set('branch', null)
+	  expect(job.get('formattedCommit')).toEqual('')
+	});
+      });
+
+      describe('formattedCompareUrl', function() {
+	it('returns an empty string when there is no compare_url', function() {
+	  expect(job.get('formattedCompareUrl')).toEqual('')
+	});
+
+	it('returns the last part in the url when there is a compare url', function() {
+	  job.set('compare_url', 'https://github.com/travis-ci/travis-ci/compare/9bc2212...9d1e44a')
+	  expect(job.get('formattedCompareUrl')).toEqual('9bc2212...9d1e44a')
+	});
+      });
+
+      describe('formattedConfig', function() {
+	it('returns the configuration formatted', function() {
+	  expect(job.get('formattedCompareUrl')).toEqual('')
+	});
+
+	it('returns the last part in the url when there is a compare url', function() {
+	  job.set('compare_url', 'https://github.com/travis-ci/travis-ci/compare/9bc2212...9d1e44a')
+	  expect(job.get('formattedCompareUrl')).toEqual('9bc2212...9d1e44a')
+	});
+      });
+
+      describe('formattedConfigValues', function() {
+
+	it('returns an empty array when there is nothing to format', function() {
+          expect(job.get('formattedConfigValues')).toEqual([]);
+	});
+
+	it('returns the config items as an array of Ember Objects', function() {
+	  job.set('config', { rvm: ['1.9.2', 'rbx'], gemfile: ['Gemfile.rails-2.3.x', 'Gemfile.rails-3.x'] });
+          config_values = $.map(job.get('formattedConfigValues'), function(item) {
+	    return item.get('value');
+	  })
+	  expect(config_values).toEqual(['1.9.2', 'rbx', 'Gemfile.rails-2.3.x', 'Gemfile.rails-3.x']);
+	});
+      });
+
+      describe('formattedLog', function() {
+	it('returns the log formatted', function() {
+	  expect(job.get('formattedLog')).toEqual('<p><a href="#/L1" id="/L1" name="L1">1</a>Done. Build script exited with: 0</p>')
+	});
+      });
+
+      describe('formattedMessage', function() {
+	it('returns the message formatted', function() {
+	  job.set('message','travis-ci is \n AWESOME :/')
+	  expect(job.get('formattedMessage')).toEqual('travis-ci is <br/> AWESOME :/')
+	});
+      });
+
+      describe('url', function() {
+	it('returns the url for this job', function() {
+	  expect(job.get('url')).toEqual('#!/travis-ci/travis-ci/jobs/9')
+	});
+      });
+
+    });
+
+    describe('methods', function() {
+	it('update', function() {
+          expect(job.get('result')).toBeNull();
+          job.update({ result: 1 })
+          expect(job.get('result')).toEqual(1);
+	});
+
       it('appendLog', function() {
-        build.set('log', 'test-1');
-        build.appendLog('test-2');
-        expect(build.get('log')).toEqual('test-1test-2');
+        job.set('log', 'test-1');
+        job.appendLog('test-2');
+        expect(job.get('log')).toEqual('test-1test-2');
       });
-    });
-  });
 
-  describe('properties', function() {
-    var repository, build, view;
+      it('subscribe', function() {
+	expect(Travis.channels.indexOf('job-9')).toEqual(-1);
+	job.subscribe()
+	expect(Travis.channels.indexOf('job-9')).toEqual(0);
+      });
 
-    beforeEach(function() {
-      repository = Test.Factory.Repository.travis();
-      build = Test.Factory.Job.single();
     });
 
-    it('commit', function() {
-      expect(build.get('formattedCommit')).toEqual('4d7621e (master)');
-    });
-
-    describe('duration', function() {
-      it("returns a '-' if the build's start time is not known", function() {
-        build.set('started_at', null);
-        expect(build.get('formattedDuration')).toEqual('-');
-      });
-
-      it("returns a human readable duration using the current time if the build's finished time is not known", function() {
-        build.set('finished_at', null);
-        expect(build.get('formattedDuration')).toEqual('more than 24 hrs');
-      });
-
-      it("returns a human readable duration if the build's start and finished times are both known", function() {
-        expect(build.get('formattedDuration')).toEqual('10 sec');
-      });
-    });
-
-    describe('finished_at', function() {
-      it("returns a '-' if the build's finished time is not known", function() {
-        build.set('finished_at', null);
-        expect(build.get('formattedFinishedAt')).toEqual('-');
-      });
-
-      it("returns a human readable time ago string if the build's finished time is known", function() {
-        spyOn($.timeago, 'now').andReturn(new Date(Date.UTC(2011, 0, 1, 4, 0, 0)).getTime());
-        expect(build.get('formattedFinishedAt')).toEqual('about 3 hours ago');
-      });
-    });
-
-    describe('config', function() {
-      it('returns "-" if the config is undefined', function() {
-        build.set('config', null);
-        expect(build.get('formattedConfig')).toEqual('-');
-      });
-
-      it('returns a  displayable config string', function() {
-        build.set('config', { rvm: ['1.9.2', 'rbx'], gemfile: ['Gemfile.rails-2.3.x', 'Gemfile.rails-3.x'] });
-        expect(build.get('formattedConfig')).toEqual('Rvm: 1.9.2, rbx, Gemfile: Gemfile.rails-2.3.x, Gemfile.rails-3.x');
-      });
-
-      it("ignores the .configured key", function() {
-        build.set('config', { '.configured': true });
-        expect(build.get('formattedConfig')).toEqual('-');
-      });
-    });
-
-    describe('message', function() {
-      it ('changes emoji to image tags', function() {
-        build.set('message', 'The :cake: is a lie');
-        expect(build.get('formattedMessage')).toEqual('The <img class="emoji" title=":cake:" alt=":cake:" src="/assets/emoji/cake.png"/> is a lie');
-      });
-
-      it ('changes multiple emoji to image tags', function() {
-        build.set('message', 'I :heart: :cake:');
-        expect(build.get('formattedMessage')).toEqual('I <img class="emoji" title=":heart:" alt=":heart:" src="/assets/emoji/heart.png"/> <img class="emoji" title=":cake:" alt=":cake:" src="/assets/emoji/cake.png"/>');
-      });
-
-      it ('does not change message without emoji', function() {
-        build.set('message', 'Issue: This is normal commit :: Something with ActiveSupport::Callbacks: remove __define_runner');
-        expect(build.get('formattedMessage')).toEqual('Issue: This is normal commit :: Something with ActiveSupport::Callbacks: remove __define_runner');
-      });
-
-      it ('shows mulitple lines commits in multiple html lines', function() {
-        build.set('message', 'First line of commit.\n\nSecond line of commit\nThird line.');
-        expect(build.get('formattedMessage')).toEqual('First line of commit.<br/><br/>Second line of commit<br/>Third line.');
-      });
-    });
   });
 });
 
