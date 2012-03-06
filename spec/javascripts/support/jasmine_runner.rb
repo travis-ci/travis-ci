1d9a90a1b31af8cd2ef37d07b4877b7b3e2e165f
Randy Morgan <randym@reallyenglish.com>
2012-03-06 17:41:27 +0900

    Finishing off jasmine:ci integration. All UI specs are now stored in the usual spec/javascripts and jasmine:ci will be called on rake test:ci.
    All green on my local, lets see what Travis has to say....

--- spec/javascripts/support/jasmine_runner.rb
+++ spec/javascripts/support/jasmine_runner.rb
@@ -1,14 +1,12 @@
 $:.unshift(ENV['JASMINE_GEM_PATH']) if ENV['JASMINE_GEM_PATH'] # for gem testing purposes
 
 require 'rubygems'
-require File.expand_path("../../../../config/environment", __FILE__)
-
-require 'jasmine'
 require 'json'
-
+require 'jasmine'
 
 jasmine_config_overrides = File.expand_path(File.join(File.dirname(__FILE__), 'jasmine_config.rb'))
 require jasmine_config_overrides if File.exist?(jasmine_config_overrides)
+
 if Jasmine::Dependencies.rspec2?
   require 'rspec'
 else
