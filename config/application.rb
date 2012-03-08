c3b423d849003b56040fd4194f0c37d4a06ad8e4
Randy Morgan <randym@reallyenglish.com>
2012-03-08 15:14:09 +0900

    quick i18n knock up with localeapp

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 Bundler.require *Rails.groups(:assets) if defined?(Bundler)
-
 module TravisCi
   class Application < Rails::Application
     config.encoding = 'utf-8'
@@ -18,6 +17,9 @@ module TravisCi
     config.active_record.default_timezone = :utc
 
     ActiveRecord::Base.include_root_in_json = false
+
+    config.i18n.default_locale = :en
+    config.i18n.load_path += Dir[Rails.root.join('locales', '*.{rb,yml}').to_s]
 
     config.middleware.use Rack::JSONP
   end
