f20dc8690f5274f138a33560d2517d970de70e44
Sven Fuchs <svenfuchs@artweb-design.de>
2012-03-09 14:31:54 +0100

    fix the schema ... hopefully

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120304000505) do
+ActiveRecord::Schema.define(:version => 20120304000502) do
 
   create_table "artifacts", :force => true do |t|
     t.text     "content"
