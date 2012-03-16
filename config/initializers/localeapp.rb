bbd443115189c4be2768877c3d7b290340c4f128
Josh Kalderimis <josh.kalderimis@gmail.com>
2012-03-16 16:54:38 +0100

    remove the localeapp initializer for now

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,5 +1,5 @@
-require 'localeapp/rails'
-
-Localeapp.configure do |config|
-  config.api_key = 'QfVzW8YumwGB20GPwgNAVK5ZEAfKSheRRQJ8qXy9dHIDRKWmEA'
-end
+# require 'localeapp/rails'
+#
+# Localeapp.configure do |config|
+#   config.api_key = 'QfVzW8YumwGB20GPwgNAVK5ZEAfKSheRRQJ8qXy9dHIDRKWmEA'
+# end
