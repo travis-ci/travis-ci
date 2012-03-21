453f1279c90dedc6abee057e9a89e3d95e031b4c
Josh Kalderimis <josh.kalderimis@gmail.com>
2012-03-21 01:10:37 +0100

    removed the png mimetype as rails already adds it

--- config/initializers/rails/mime_types.rb
+++ config/initializers/rails/mime_types.rb
@@ -3,5 +3,3 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
-
-Mime::Type.register 'image/png', :png
