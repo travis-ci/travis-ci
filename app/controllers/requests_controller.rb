4db8246cba63a7c598c2414bbbb18897c434e0da
Sven Fuchs <svenfuchs@artweb-design.de>
2012-04-16 22:03:45 +0200

    use request.headers for the event_type?

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,11 +2,15 @@ class RequestsController < ApplicationController
   # also responds to POST /builds legacy route
   def create
     ActiveSupport::Notifications.publish('github.request.deprecated.api', params)
-    Request.create_from(request.env['HTTP_X_GITHUB_EVENT'], params[:payload], api_token)
+    Request.create_from(event_type, params[:payload], api_token)
     render :nothing => true
   end
 
   protected
+
+    def event_type
+      request.headers['HTTP_X_GITHUB_EVENT'] || 'push'
+    end
 
     def api_token
       credentials = ActionController::HttpAuthentication::Basic.decode_credentials(request)
