ea8015da041c2814f4961ac5b4144c8a562e976d
Mathias Meyer <meyer@paperplanes.de>
2012-04-16 12:36:29 +0200

    Switch to Metriks gems. Replace logging with lograge.

--- lib/notifications.rb
+++ lib/notifications.rb
@@ -2,21 +2,13 @@ require 'active_support/notifications'
 
 module Travis
   class Metrics
-    def initialize
-      @env = ENV['ENV'] || "production"
-    end
-
     def metric_name(name)
-      "travis.#{@env}.#{name}"
+      "travis.#{name}"
     end
 
     def subscribe
-      ActiveSupport::Notifications.subscribe(%r{^github\.requests.*}) do |name, source, payload|
-        if source
-          Metriks.meter(metric_name(name), source).mark
-        else
-          Metriks.meter(metric_name(name)).mark
-        end
+      ActiveSupport::Notifications.subscribe(%r{^github\.requests.*}) do |name, payload|
+        Metriks.meter(metric_name(name)).mark
       end
     end
   end
