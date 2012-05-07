d90f26a869ebb9375cdac80de91273c1bef1f93f
Sven Fuchs <svenfuchs@artweb-design.de>
2012-05-07 17:09:07 +0200

    remove rabl initializer

--- config/initializers/rabl.rb
+++
@@ -1,8 +0,0 @@
-require 'rabl'
-
-Rabl.configure do |config|
-  config.include_json_root = false
-  config.include_xml_root  = false
-  config.xml_options = { :dasherize => false, :skip_types => true }
-end
-
