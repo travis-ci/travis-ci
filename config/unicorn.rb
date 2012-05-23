7ab22530c4334af41dd2a81941e0e22aaeda6b7e
Sven Fuchs <svenfuchs@artweb-design.de>
2012-05-23 14:47:52 +0200

    increase unicorn timeout

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 # http://michaelvanrooijen.com/articles/2011/06/01-more-concurrency-on-a-single-heroku-dyno-with-the-new-celadon-cedar-stack/
 
 worker_processes 3 # amount of unicorn workers to spin up
-timeout 15         # restarts workers that hang for 15 seconds
+timeout 30         # restarts workers that hang for 15 seconds
 
 preload_app true
 
