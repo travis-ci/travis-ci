d6adc06f52c5b0f733c47b629d73cb7594fa0449
Sven Fuchs <svenfuchs@artweb-design.de>
2012-05-26 22:11:26 +0200

    change routes so we actually can switch the api version using accept headers or query param

--- lib/api_constraints.rb
+++ lib/api_constraints.rb
@@ -1,11 +1,33 @@
 class ApiConstraints
+  attr_reader :options
+
   def initialize(options)
-    @version = options[:version]
-    @default = options[:default]
+    @options = options
   end
 
-  def matches?(req)
-    p req
-    @default || req.headers['Accept'].include?("application/vnd.travis-ci.org; version=#{@version}")
+  def matches?(request)
+    default? || matches_accept_header?(request) || matches_query_param?(request)
   end
+
+  private
+
+    def default?
+      !!options[:default]
+    end
+
+    def matches_accept_header?(request)
+      request.headers.key?('Accept') && request.headers['Accept'].include?(accept_header)
+    end
+
+    def matches_query_param?(request)
+      request.params[:version].try(:to_i) == version.to_i
+    end
+
+    def version
+      options[:version]
+    end
+
+    def accept_header
+      "application/vnd.travis-ci.#{version}+json"
+    end
 end
