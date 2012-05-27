587676590072bc081ef1172dd6b0a85cd935c162
Sven Fuchs <svenfuchs@artweb-design.de>
2012-05-27 03:18:43 +0200

    remove stale routes and add routing specs

--- config/routes.rb
+++ config/routes.rb
@@ -29,11 +29,9 @@ TravisCi::Application.routes.draw do
     end
 
     constraints :owner_name => /[^\/]+/, :name => /[^\/]+/ do
-      get ":owner_name/:name.json",            :to => 'repositories#show', :format => :json
-      get ":owner_name/:name.png",             :to => 'repositories#show', :format => :png
-      get ":owner_name/:name/builds.json",     :to => 'builds#index',      :format => :json
-      get ":owner_name/:name/builds/:id.json", :to => 'builds#show',       :format => :json
-      get ":owner_name/:name/cc.xml",          :to => 'repositories#show', :format => :xml, :schema => 'cctray'
+      get ":owner_name/:name.json",   :to => 'repositories#show', :format => :json
+      get ":owner_name/:name.png",    :to => 'repositories#show', :format => :png
+      get ":owner_name/:name/cc.xml", :to => 'repositories#show', :format => :xml, :schema => 'cctray'
     end
   end
 
