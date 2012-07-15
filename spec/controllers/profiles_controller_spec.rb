ac3617da4d71812cd72c38badebb8eefd0474cf1
Sven Fuchs <svenfuchs@artweb-design.de>
2012-07-15 18:09:22 +0200

    stop syncing users on signin, instead have a sync action that we can POST to

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -16,15 +16,52 @@ describe ProfilesController do
   end
 
   describe 'POST :update' do
-
     it 'updates the locale for the user profile' do
-      post :update, :user=>{:locale => :ja}
+      post :update, :user => { :locale => :ja }
       I18n.locale.should == :ja
       controller.current_user.locale.should == "ja"
       session[:locale].should == :ja
       response.should redirect_to :profile
     end
-
   end
 
+  describe 'POST :sync' do
+    let(:publisher) { stub('publisher', :publish => true) }
+
+    before :each do
+      Travis::Amqp::Publisher.stubs(:new).returns(publisher)
+    end
+
+    describe 'given the current user is not being synced' do
+      before :each do
+        user.update_attribute(:is_syncing, false)
+      end
+
+      it 'schedules a sync job' do
+        publisher.expects(:publish).with({ user_id: user.id }, type: 'sync')
+        post :sync
+      end
+
+      it 'sets the current user to being synced' do
+        post :sync
+        user.reload.is_syncing.should be_true
+      end
+    end
+
+    describe 'given the current user is being synced' do
+      before :each do
+        user.update_attribute(:is_syncing, true)
+      end
+
+      it 'does not schedule a sync job' do
+        publisher.expects(:publish).never
+        post :sync
+      end
+
+      it 'does not set the current user to being synced' do
+        user.expects(:update_attribute).never
+        post :sync
+      end
+    end
+  end
 end
