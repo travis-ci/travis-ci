c9763ac980a21472ddd0071d6f99d4c80c3fb4b6
Sven Fuchs <svenfuchs@artweb-design.de>
2012-07-16 15:29:52 +0200

    work around broken rails json rendering, need to extract this to travis-core/api

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,7 +9,24 @@ class ProfilesController < ApplicationController
   respond_to :html, :only => :show
 
   def show
-    respond_with user
+    # TODO extract json to travis-core/api and use the json responder
+    respond_to do |format|
+      format.html do
+        @user = user
+        render :show
+      end
+      format.json do
+        render :json => {
+          login: user.login,
+          name: user.name,
+          gravatar_id: user.gravatar_id,
+          email: user.email,
+          locale: user.locale,
+          is_syncing: user.is_syncing,
+          synced_at: user.synced_at
+        }
+      end
+    end
   end
 
   def update
