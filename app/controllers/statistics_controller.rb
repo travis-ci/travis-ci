583d21b0d76c82495710fa15ee04dd12697d6bb0
Sven Fuchs <me@svenfuchs.com>
2012-09-11 11:06:57 +0200

    disable statistics fro now

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -4,7 +4,7 @@ class StatisticsController < ApplicationController
   layout 'simple'
 
   def index
-    @repo_graph_stats = Statistics.daily_repository_counts
-    @test_graph_stats = Statistics.daily_tests_counts
+    # @repo_graph_stats = Statistics.daily_repository_counts
+    # @test_graph_stats = Statistics.daily_tests_counts
   end
 end
