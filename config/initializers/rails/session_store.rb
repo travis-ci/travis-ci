b1a7a8bacb67823ad8ce31412bce6b61d263131b
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-09-18 13:10:17 +0200

    keep session around after browser session (fixes log out issue on Safari)

--- config/initializers/rails/session_store.rb
+++ config/initializers/rails/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-TravisCi::Application.config.session_store :cookie_store, :key => '_travis_session'
+TravisCi::Application.config.session_store :cookie_store, :key => '_travis_session', :expire_after => 1.day
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
